3668491fcf28bf53df289b113533376b68c5212b
Zach Hindes <zach.hindes@ni.com>
2022-01-19 14:31:30 -0600

    issue 36: no version information available

--- nidaqmx/__init__.py
+++ nidaqmx/__init__.py
@@ -8,4 +8,11 @@ from nidaqmx.scale import Scale
 from nidaqmx.task import Task
 from nidaqmx._task_modules.read_functions import CtrFreq, CtrTick, CtrTime
 
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
+__version__ = version(__name__)
+
 __all__ = ['errors', 'scale', 'stream_readers', 'stream_writers', 'task']
