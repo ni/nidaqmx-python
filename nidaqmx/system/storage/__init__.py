56206dbf5f1bee3d273e49d9df9f36e44a58999a
Zach Hindes <zach.hindes@ni.com>
2022-02-02 10:34:51 -0600

    remove future imports (#137)
    * remove future imports
    
    * remove leading blank lines

--- nidaqmx/system/storage/__init__.py
+++ nidaqmx/system/storage/__init__.py
@@ -1,8 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 from nidaqmx.system.storage.persisted_channel import PersistedChannel
 from nidaqmx.system.storage.persisted_scale import PersistedScale
 from nidaqmx.system.storage.persisted_task import PersistedTask
--- nidaqmx/system/storage/persisted_channel.py
+++ nidaqmx/system/storage/persisted_channel.py
@@ -1,8 +1,3 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-from __future__ import unicode_literals
-
 import ctypes
 
 from nidaqmx._lib import lib_importer, ctypes_byte_str, c_bool32
