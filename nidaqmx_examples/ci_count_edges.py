4d51cb13a979d7ed31eeaa0c6b83630e84975fe8
Zach Hindes <zach.hindes@ni.com>
2022-02-09 09:06:22 -0600

    more

--- nidaqmx_examples/ci_count_edges.py
+++ nidaqmx_examples/ci_count_edges.py
@@ -8,6 +8,8 @@ pp = pprint.PrettyPrinter(indent=4)
 with nidaqmx.Task() as task:
     task.ci_channels.add_ci_count_edges_chan("Dev1/ctr0")
 
+    task.start()
+
     print('1 Channel 1 Sample Read: ')
     data = task.read()
     pp.pprint(data)
