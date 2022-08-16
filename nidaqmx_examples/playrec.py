4a865f4bcd0f06e9c2098e96f96ff04d1845c650
fhchl <fhchl@users.noreply.github.com>
2022-08-16 12:14:08 +0200

    improved example

--- nidaqmx_examples/playrec.py
+++ nidaqmx_examples/playrec.py
@@ -7,20 +7,13 @@ from nidaqmx.constants import WAIT_INFINITELY
 
 def query_devices():
   local = ni.system.System.local()
-  driver = local.driver_version
-
-  print(f'DAQmx {0}.{1}.{2}'.format(
-    driver.major_version, driver.minor_version, driver.update_version))
-
   for device in local.devices:
-    print('Device Name: {0}, Product Category: {1}, Product Type: {2}'.format(
-      device.name, device.product_category, device.product_type))
+    print(f'Device Name: {device.name}, Product Type: {device.product_type}')
+    print('Input channels:', [chan.name for chan in device.ai_physical_chans])
+    print('Output channels:', [chan.name for chan in device.ao_physical_chans])
 
 
-def playrec(
-  data, samplerate=96000, input_mapping=['Dev2/ai0', 'Dev2/ai1', 'Dev2/ai2'],
-  output_mapping=['Dev2/ao0']
-):
+def playrec(data, samplerate, input_mapping, output_mapping):
   """Simultaneous playback and recording though NI device.
 
   Parameters:
@@ -40,26 +33,24 @@ def playrec(
     Recorded data
 
   """
-  max_out_range = 3.5 # output range of USB-4431
-  max_in_range = 10   # input range of USB-4431
-  max_outdata = np.max(np.abs(outdata))
-  if max_outdata > max_out_range:
-    raise ValueError(
-      f"outdata amplitude ({max_outdata:.2f}) larger than allowed range"
-      f"(+-{max_out_range:.2f}).")
-
-  outdata = np.asarray(outdata).T
-  nsamples = outdata.shape[1]
+  devices = ni.system.System.local().devices
+  data = np.asarray(data).T
+  nsamples = data.shape[1]
 
   with ni.Task() as read_task, ni.Task() as write_task:
-    for o in output_mapping:
-      aochan = write_task.ao_channels.add_ao_voltage_chan(o)
-      aochan.ao_max = max_out_range
-      aochan.ao_min = -max_out_range
+    for i, o in enumerate(output_mapping):
+      aochan = write_task.ao_channels.add_ao_voltage_chan(
+        o,
+        min_val=devices[o].ao_voltage_rngs[0],
+        max_val=devices[o].ao_voltage_rngs[1]
+      )
+      min_data, max_data = np.min(data[i]), np.max(data[i])
+      if ((max_data > aochan.ao_max) | (min_data < aochan.ao_min)).any():
+        raise ValueError(
+          f"Data range ({min_data:.2f}, {max_data:.2f}) exceeds output range of "
+          f"{o} ({aochan.ao_min:.2f}, {aochan.ao_max:.2f}).")
     for i in input_mapping:
-      aichan = read_task.ai_channels.add_ai_voltage_chan(i)
-      aichan.ai_min = -max_in_range
-      aichan.ai_max = max_in_range
+      read_task.ai_channels.add_ai_voltage_chan(i)
 
     for task in (read_task, write_task):
       task.timing.cfg_samp_clk_timing(rate=samplerate, source='OnboardClock',
@@ -68,16 +59,26 @@ def playrec(
     # trigger write_task as soon as read_task starts
     write_task.triggers.start_trigger.cfg_dig_edge_start_trig(
             read_task.triggers.start_trigger.term)
-    write_task.write(outdata, auto_start=False)
-    write_task.start()  # write_task doesn't start at read_task's start_trigger
-                        # without this
-    indata = read_task.read(nsamples, timeout=WAIT_INFINITELY) # do not time out
-                                                               # for long inputs
+    # squeeze as Task.write expects 1d array for 1 channel
+    write_task.write(data.squeeze(), auto_start=False)
+    # write_task doesn't start at read_task's start_trigger without this
+    write_task.start()  
+    # do not time out for long inputs
+    indata = read_task.read(nsamples, timeout=WAIT_INFINITELY) 
 
   return np.asarray(indata).T
 
 
 if __name__ == '__main__':
+  query_devices()
+  # Prints in this example:
+  #   Device Name: Dev2, Product Type: USB-4431
+  #   Input channels: ['Dev2/ai0', 'Dev2/ai1', 'Dev2/ai2', 'Dev2/ai3']
+  #   Output channels: ['Dev2/ao0']
+
   # excite through one output and record at three inputs
-  outdata = np.random.normal(size=(1000, 1))*0.01
-  indata = playrec(outdata)
+  outdata = np.random.normal(size=(5000, 1))*0.01
+  indata = playrec(outdata,
+                   samplerate=96000,
+                   input_mapping=['Dev2/ai0', 'Dev2/ai1', 'Dev2/ai2'],
+                   output_mapping=['Dev2/ao0'])
