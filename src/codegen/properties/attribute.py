5a702af05e79b0da8c7de5a41a96ab85eec97c8e
Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>
2023-02-17 16:57:21 +0530

    feat: add parameters as a module
    Signed-off-by: Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>

--- src/codegen/properties/attribute.py
+++ src/codegen/properties/attribute.py
@@ -1,4 +1,6 @@
-class Attribute():
+from codegen.properties.parameter import Parameter
+
+class Attribute:
     
     def __init__(self, id, attribute_metadata):
         self._id  = id
@@ -36,7 +38,8 @@ class Attribute():
     @property
     def id(self):
         """
-        Represents a unique integer value that represents an attribute.
+        str: Represents a unique integer value that represents an attribute.
+
         This is the key for the attribute itself
         """
         return self._id
@@ -45,19 +48,18 @@ class Attribute():
     @property
     def access(self):
         """
-        Specifies if the attribute is read/write.
-        Supported Values
-        - `read`: The attribute is readable.(get)
-        - `write`: The attribute is writeable or can be set with new values.(set)
-        - `read-write`: The attribute can be both be read and written.(get and set)
+        str: Specifies if the attribute is read/write.
+
         This is used to decide the generation of getters and setters of a property representing the attribute.
+        The possible values can be read, write or read-write.
         """
         return self._access
 
     @property
     def name(self):
         """
-        Name of the attribute.
+        str: Name of the attribute.
+
         This name is used to generate the property name.
         """
         return self._name
@@ -65,15 +67,17 @@ class Attribute():
     @property
     def resettable(self):
         """
-        `True`: This attribute can be reset back to default. This is also used to decide if a deleter has to be generated for the property.
-        `False`: This attribute cannot be reset back to default.
+        bool: This attribute can be reset back to default.
+        
+        This is also used to decide if a deleter has to be generated for the property.
         """
         return self._resettable
 
     @property
     def type(self):
         """
-        Data type of the attribute.
+        str: Data type of the attribute.
+
         Here `enum` types are always represented as integers.
         """
         return self._type
@@ -82,7 +86,8 @@ class Attribute():
     @property
     def has_explicit_write_buffer_size(self):
         """
-        Specifies if an explicit write buffer size has to be provided when making the c function calls for the attribute.
+        bool: Specifies if an explicit write buffer size has to be provided when making the c function calls for the attribute.
+        
         If True then an additional uint parameter would be provided when calling the c function to mention the buffer size.
         """
         if not hasattr(self,"_has_explicit_write_buffer_size"):
@@ -92,8 +97,8 @@ class Attribute():
     @property
     def object_has_factory(self):
         """
-        This key is only present when `is_object` value is `True`.
-        'True' if the object has a factory implementation.
+        bool: If attribute is of an object type, this specifies if it uses a factory method.
+        
         If the value is `True` then the `_factory` method is used for instantiation of the object.
         """
         return self._object_has_factory
@@ -101,14 +106,15 @@ class Attribute():
     @property
     def is_enum(self):
         """
-        Represents if the attribute is an enum or not.
+        booL: Represents if the attribute is an enum or not.
         """
         return self._is_enum
     
     @property
     def enum(self):
         """
-        The enum type the attribute represents.
+        str: The enum type the attribute represents.
+
         This key will only be available for an enum type attribute.
         During code generation an attribute would be considered as an enum if it contains this key.
         """
@@ -117,7 +123,8 @@ class Attribute():
     @property
     def handle_parameters(self):
         """
-        A list of parameters that represent handles that the attribute is part of.
+        str: A list of parameters that represent handles that the attribute is part of.
+
         These are used when defining the c function parameters, these are usually the first set of inputs to the function.
         """
         return self._handle_parameters
@@ -125,8 +132,8 @@ class Attribute():
     @property
     def object_constructor_params(self):
         """
-        This key is only present when `is_object` value is `True`.
-        The additional parameters that needs to included in the object creation apart from the attribute value.
+        str: This contains the additional parameters that needs to included in the object creation.
+        
         During python code generation, these parameters are added as initial inputs when creating the object.
         """
         return self._object_constructor_params
@@ -134,7 +141,8 @@ class Attribute():
     @property
     def python_class_name(self):
         """
-        The name of the python class this attribute belongs to.
+        str: The name of the python class this attribute belongs to.
+        
         This is used to determine which attribute goes to which class when generating the python code.
         """
         return self._python_class_name
@@ -142,17 +150,15 @@ class Attribute():
     @property
     def is_object(self):
         """
-        'True': The attribute represents a known object.
-        'False' : The attribute represents the `type` value as usual.
-        During python code generation, this is used to determine if the value has to be used as an object in getters and setters.
+        str: This is used to determine if the value has to be used as an object in getters and setters.
         """
         return self._is_object
 
     @property
     def object_type(self):
         """
-        This key is only present when `is_object` value is `True`.
-        The name of the object.
+        str: The name of the object.
+        
         During code generation, this is used to instantiate the object.
         """
         return self._object_type
@@ -160,7 +166,8 @@ class Attribute():
     @property
     def c_function_name(self):
         """
-        The name of the c function to be called when using the attribute.
+        str: The name of the c function to be called when using the attribute.
+        
         This name will be prefixed with `DAQmxSet', `DAQmxGet` and `DAQmxReset` for using in getters, setters and deleters respectively.
         """
         return self._c_function_name
@@ -168,26 +175,22 @@ class Attribute():
     @property
     def calling_convention(self):
         """
-        The calling convention to be followed when using the c functions.
-        Possible values
-        - 'StdCall'
-        - 'Cdecl'
+        str: The calling convention to be followed when using the c functions.
         """
         return self._calling_convention
 
     @property
     def is_list(self):
         """
-        'True': If the attribute is a list.
-        'False': Id the attribute is a scalar value.
+        bool: Determines if the attribute is of type list or not
         """
         return self._is_list
 
     @property
     def bitfield_enum(self):
         """
-        The name of the bitfield enum that the attribute represents
-        The value is `N/A` when it is not a bitField Enum.
+        str: The name of the bitfield enum that the attribute represents
+        
         During code generation in python, this will be used to decide if the `enum_to_bitfield_list` method needs to called in the getter when returning the value.
         """
         if(self._bitfield_enum == "N/A"):
@@ -197,7 +200,8 @@ class Attribute():
     @property
     def has_explicit_read_buffer_size(self):
         """
-        Specifies if an explicit read buffer size has to be provided when making the c function calls for the attribute.
+        bool: Specifies if an explicit read buffer size has to be provided when making the c function calls for the attribute.
+        
         If True then an additional uint parameter would be provided when calling the c function to mention the buffer size.
         """
         return self._has_explicit_read_buffer_size
@@ -205,7 +209,8 @@ class Attribute():
     @property
     def read_buffer_size(self):
         """
-        The read buffer size to be used when calling the c function.
+        str: The read buffer size to be used when calling the c function.
+        
         This key would only be applicable if `has_explicit_read_buffer_size` is `True`.
         In case the `has_explicit_read_buffer_size` is `True` and this key is not present, then the ivi dance method is used to get the buffer size.
         """
@@ -216,7 +221,8 @@ class Attribute():
     @property
     def description(self):
         """
-        The description of the attribute.
+        str: The description of the attribute.
+        
         This will be used to define the docstring of the attribute when generating the code.
         """
         return self._description
@@ -224,7 +230,8 @@ class Attribute():
     @property
     def python_data_type(self):
         """
-        The python data_type of the attribute.
+        str: The python data_type of the attribute.
+        
         Currently this is used in the generation of the doc string for the attribute.
         """
         return self._python_data_type
@@ -232,45 +239,8 @@ class Attribute():
     @property
     def ctypes_data_type(self):
         """
-        The type of the attribute as per the ctypes definition in python.
+        str: The type of the attribute as per the ctypes definition in python.
+        
         This is used to provide the type of the attribute when making c function calls in python.
         """
         return self._ctypes_data_type
-
-class Parameter():
-    def __init__(self, name, parameter_metadata):
-        self._handle_name = name
-        self._accessor = parameter_metadata["accessor"]
-        self._ctypes_data_type = parameter_metadata["ctypes_data_type"]
-        self._cvi_name = parameter_metadata["cvi_name"]
-
-    @property
-    def handle_name(self):
-        """
-        The key of the parameter.
-        """
-        return self._handle_name
-
-    @property
-    def accessor(self):
-        """
-        Defines how to access the handle parameter.
-        This value would be directly substituted when trying to use the handle parameter
-        """
-        return self._accessor
-
-    @property
-    def ctypes_data_type(self):
-        """
-        Defines the ctypes data_type of the handle parameter.
-        This is used when mentioning the data_type of the handle parameter.
-        """
-        return self._ctypes_data_type
-
-    @property
-    def cvi_name(self):
-        """
-        The cvi name of the parameter.
-        This is kept for the gRPC client implementation.
-        """
-        return self._cvi_name
---
+++ src/codegen/properties/parameter.py
@@ -0,0 +1,40 @@
+class Parameter:
+    def __init__(self, name, parameter_metadata):
+        self._handle_name = name
+        self._accessor = parameter_metadata["accessor"]
+        self._ctypes_data_type = parameter_metadata["ctypes_data_type"]
+        self._cvi_name = parameter_metadata["cvi_name"]
+
+    @property
+    def handle_name(self):
+        """
+        str: The key of the parameter.
+        """
+        return self._handle_name
+
+    @property
+    def accessor(self):
+        """
+        str: Defines how to access the handle parameter.
+
+        This value would be directly substituted when trying to use the handle parameter
+        """
+        return self._accessor
+
+    @property
+    def ctypes_data_type(self):
+        """
+        str: Defines the ctypes data_type of the handle parameter.
+
+        This is used when mentioning the data_type of the handle parameter.
+        """
+        return self._ctypes_data_type
+
+    @property
+    def cvi_name(self):
+        """
+        str: The cvi name of the parameter.
+        
+        This is kept for the gRPC client implementation.
+        """
+        return self._cvi_name
