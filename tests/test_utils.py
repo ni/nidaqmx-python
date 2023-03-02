5cedebae56ce071a92863c39863e1210b9a4b89e
Zach Hindes <zach.hindes@ni.com>
2023-03-02 16:27:37 -0600

    more tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,20 +9,23 @@ class TestUtils(object):
     Contains a collection of pytest tests that validate the utilities
     functionality in the NI-DAQmx Python API.
     """
+    def test_basic_flatten_flatten_and_unflatten(self):
+        unflattened_channels = ['Dev1/ai0', 'Dev1/ai1', 'Dev1/ai2','Dev1/ai4', 'Dev2/ai0']
+        flattened_channels = 'Dev1/ai0:2,Dev1/ai4,Dev2/ai0'
+        assert flatten_channel_string(unflattened_channels) == flattened_channels
+        assert unflatten_channel_string(flattened_channels) == unflattened_channels
 
-    def test_flatten_channel_string(self):
-        channels = ['Dev1/ai0', 'Dev1/ai1', 'Dev1/ai3', 'Dev2/ai0']
-        flattened_channels = 'Dev1/ai0:1,Dev1/ai3,Dev2/ai0'
-        assert flatten_channel_string(channels) == flattened_channels
+    def test_backwards_flatten_flatten_and_unflatten(self):
+        unflattened_channels = ['Dev1/ai2', 'Dev1/ai1', 'Dev1/ai0', 'Dev1/ai4', 'Dev2/ai0']
+        flattened_channels = 'Dev1/ai2:0,Dev1/ai4,Dev2/ai0'
+        assert flatten_channel_string(unflattened_channels) == flattened_channels
+        assert unflatten_channel_string(flattened_channels) == unflattened_channels
 
-        assert flatten_channel_string([]) == ''
-
-    def test_unflatten_channel_string(self):
-        channels = ['Dev1/ai0', 'Dev1/ai1', 'Dev1/ai3', 'Dev2/ai0']
-        flattened_channels = 'Dev1/ai0:1,Dev1/ai3,Dev2/ai0'
-        assert unflatten_channel_string(flattened_channels) == channels
-
-        assert unflatten_channel_string('') == []
+    def test_empty_flatten_flatten_and_unflatten(self):
+        unflattened_channels = []
+        flattened_channels = ''
+        assert flatten_channel_string(unflattened_channels) == flattened_channels
+        assert unflatten_channel_string(flattened_channels) == unflattened_channels
 
     def test_leading_zeros_flatten_and_unflatten(self):
         unflattened_channels = ["EV01", "EV02"]
