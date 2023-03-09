554a3afbc89e9af0b888b9ed7fa47776c76be5d3
Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>
2023-03-09 17:47:55 +0530

    fix: lint errors in test utils
    Signed-off-by: Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,4 @@
 """Tests for validating utilities functionality."""
-import random
 
 from nidaqmx.utils import flatten_channel_string, unflatten_channel_string
 
@@ -9,29 +8,31 @@ class TestUtils(object):
 
     These validate the utilities functionality in the NI-DAQmx Python API.
     """
-    Contains a collection of pytest tests that validate the utilities
-    functionality in the NI-DAQmx Python API.
-    """
+
     def test_basic_flatten_flatten_and_unflatten(self):
-        unflattened_channels = ['Dev1/ai0', 'Dev1/ai1', 'Dev1/ai2','Dev1/ai4', 'Dev2/ai0']
-        flattened_channels = 'Dev1/ai0:2,Dev1/ai4,Dev2/ai0'
+        """Test to validate flatten and unflatten channel string function."""
+        unflattened_channels = ["Dev1/ai0", "Dev1/ai1", "Dev1/ai2", "Dev1/ai4", "Dev2/ai0"]
+        flattened_channels = "Dev1/ai0:2,Dev1/ai4,Dev2/ai0"
         assert flatten_channel_string(unflattened_channels) == flattened_channels
         assert unflatten_channel_string(flattened_channels) == unflattened_channels
 
     def test_backwards_flatten_flatten_and_unflatten(self):
-        unflattened_channels = ['Dev1/ai2', 'Dev1/ai1', 'Dev1/ai0', 'Dev1/ai4', 'Dev2/ai0']
-        flattened_channels = 'Dev1/ai2:0,Dev1/ai4,Dev2/ai0'
+        """Test to validate unflatten and flatten channel string function."""
+        unflattened_channels = ["Dev1/ai2", "Dev1/ai1", "Dev1/ai0", "Dev1/ai4", "Dev2/ai0"]
+        flattened_channels = "Dev1/ai2:0,Dev1/ai4,Dev2/ai0"
         assert flatten_channel_string(unflattened_channels) == flattened_channels
         assert unflatten_channel_string(flattened_channels) == unflattened_channels
 
     def test_empty_flatten_flatten_and_unflatten(self):
+        """Test to validate flatten and unflatten empty channel."""
         unflattened_channels = []
-        flattened_channels = ''
+        flattened_channels = ""
         assert flatten_channel_string(unflattened_channels) == flattened_channels
         assert unflatten_channel_string(flattened_channels) == unflattened_channels
 
     def test_leading_zeros_flatten_and_unflatten(self):
+        """Test to validate leading zeros in flatten and unflatten string function."""
         unflattened_channels = ["EV01", "EV02"]
-        flattened_channels = 'EV01:02'
+        flattened_channels = "EV01:02"
         assert flatten_channel_string(unflattened_channels) == flattened_channels
         assert unflatten_channel_string(flattened_channels) == unflattened_channels
