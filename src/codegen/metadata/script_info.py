515755d046c5f546e4daa6f7abb64998de71cd75
Battu Mounika <battu.mounika@solitontech.com>
2023-03-21 16:54:42 +0530

    Changed directory path

--- src/codegen/metadata/script_info.py
+++ src/codegen/metadata/script_info.py
@@ -5,8 +5,8 @@ script_info = {
             "templateFile": "_task_modules\\channels\\ai_channel.py.mako",
         },
         {
-            "relativeOutputPath": "_task_modules\\channels\\ai_channel_collection.py",
-            "templateFile": "_task_modules\\channels\\ai_channel_collection.py.mako",
+            "relativeOutputPath": "_task_modules\\ai_channel_collection.py",
+            "templateFile": "_task_modules\\ai_channel_collection.py.mako",
         }
     ]
 }
