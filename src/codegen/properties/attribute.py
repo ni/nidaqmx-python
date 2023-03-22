1b71b3925d1cf3e6c0e48f5a533ca23eed1c0ed1
Sam Chrisvin <104556074+samchris007@users.noreply.github.com>
2023-03-22 18:34:16 +0530

    [PR2] [Channel] Generate ci_channel, co_channel, di_channel, do_channel class files (#222)
    * Update to latest scrapigen data
    
    * generate ao_channel.py.
    
    * Resolve comments
    
    * Fix formatting using black
    
    * Update logic in property generation
    
    * Generate other channel classes
    
    * Fix formatting using black
    
    * Update script_info.py for generating do_channel.py
    
    * Fix addition of property in generating ao_channel.py
    
    * Resolve comments
    
    ---------
    
    Co-authored-by: Subash Suresh <subash.suresh@solitontech.com>

--- src/codegen/properties/attribute.py
+++ src/codegen/properties/attribute.py
@@ -8,6 +8,8 @@ class Attribute:
     ATTRIBUTE_CHANGE_SET = {
         "ai_custom_scale_name": "ai_custom_scale",
         "ao_custom_scale_name": "ao_custom_scale",
+        "ci_custom_scale_name": "ci_custom_scale",
+        "ci_dup_count_prevent": "ci_dup_count_prevention"
     }
 
     def __init__(self, id, attribute_metadata, enum_merge_set):
