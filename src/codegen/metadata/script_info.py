4b9aef09bcbd01dd25d226a861c7463ed006aad8
Battu Mounika <battu.mounika@solitontech.com>
2023-03-27 15:51:02 +0530

    Added trigger files to script info file

--- src/codegen/metadata/script_info.py
+++ src/codegen/metadata/script_info.py
@@ -1,40 +1,56 @@
 script_info = {
     "modules": [
-        # {
-        #     "relativeOutputPath": "_task_modules\\channels\\channel.py",
-        #     "templateFile": "_task_modules\\channels\\channel.py.mako",
-        # },
-        # {
-        #     "relativeOutputPath": "_task_modules\\channels\\ai_channel.py",
-        #     "templateFile": "_task_modules\\channels\\ai_channel.py.mako",
-        # },
-        # {
-        #     "relativeOutputPath": "_task_modules\\channels\\ao_channel.py",
-        #     "templateFile": "_task_modules\\channels\\ao_channel.py.mako",
-        # },
-        # {
-        #     "relativeOutputPath": "_task_modules\\channels\\ci_channel.py",
-        #     "templateFile": "_task_modules\\channels\\ci_channel.py.mako",
-        # },
-        # {
-        #     "relativeOutputPath": "_task_modules\\channels\\co_channel.py",
-        #     "templateFile": "_task_modules\\channels\\co_channel.py.mako",
-        # },
-        # {
-        #     "relativeOutputPath": "_task_modules\\channels\\di_channel.py",
-        #     "templateFile": "_task_modules\\channels\\di_channel.py.mako",
-        # },
-        # {
-        #     "relativeOutputPath": "_task_modules\\channels\\do_channel.py", 
-        #     "templateFile": "_task_modules\\channels\\do_channel.py.mako",
-        # },
-        # {
-        #     "relativeOutputPath": "_task_modules\\ai_channel_collection.py",
-        #     "templateFile": "_task_modules\\ai_channel_collection.py.mako",
-        # },
+        {
+            "relativeOutputPath": "_task_modules\\channels\\channel.py",
+            "templateFile": "_task_modules\\channels\\channel.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\channels\\ai_channel.py",
+            "templateFile": "_task_modules\\channels\\ai_channel.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\channels\\ao_channel.py",
+            "templateFile": "_task_modules\\channels\\ao_channel.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\channels\\ci_channel.py",
+            "templateFile": "_task_modules\\channels\\ci_channel.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\channels\\co_channel.py",
+            "templateFile": "_task_modules\\channels\\co_channel.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\channels\\di_channel.py",
+            "templateFile": "_task_modules\\channels\\di_channel.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\channels\\do_channel.py", 
+            "templateFile": "_task_modules\\channels\\do_channel.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\ai_channel_collection.py",
+            "templateFile": "_task_modules\\ai_channel_collection.py.mako",
+        },
         {
             "relativeOutputPath": "_task_modules\\triggering\\arm_start_trigger.py",
             "templateFile": "_task_modules\\triggering\\arm_start_trigger.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\triggering\\handshake_trigger.py",
+            "templateFile": "_task_modules\\triggering\\handshake_trigger.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\triggering\\pause_trigger.py",
+            "templateFile": "_task_modules\\triggering\\pause_trigger.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\triggering\\reference_trigger.py",
+            "templateFile": "_task_modules\\triggering\\reference_trigger.py.mako",
+        },
+        {
+            "relativeOutputPath": "_task_modules\\triggering\\start_trigger.py",
+            "templateFile": "_task_modules\\triggering\\start_trigger.py.mako",
         },
     ]
 }
