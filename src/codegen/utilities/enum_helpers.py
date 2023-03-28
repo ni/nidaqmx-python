3cca3015c3d4eedc38ec7117b4e2be2e24999ae0
Battu Mounika <battu.mounika@solitontech.com>
2023-03-28 21:02:55 +0530

    Updated enum merge set

--- src/codegen/utilities/enum_helpers.py
+++ src/codegen/utilities/enum_helpers.py
@@ -10,7 +10,7 @@ ENUM_MERGE_SET = {
     "AutoZeroType": ["AutoZeroType", "AutoZeroType1"],
     "BridgeConfiguration": ["BridgeConfiguration", "BridgeConfiguration1"],
     "CJCSource": ["CJCSource1", "CJCSource"],
-    "Coupling": ["Coupling1", "Coupling"],
+    "Coupling": ["Coupling1", "Coupling2", "Coupling"],
     "CurrentShuntResistorLocation": [
         "CurrentShuntResistorLocation",
         "CurrentShuntResistorLocation1",
@@ -19,6 +19,7 @@ ENUM_MERGE_SET = {
     "DataJustification": ["DataJustification", "DataJustification1"],
     "DigitalWidthUnits": [
         "DigitalWidthUnits",
+        "DigitalWidthUnits1",
         "DigitalWidthUnits2",
         "DigitalWidthUnits3",
         "DigitalWidthUnits4",
