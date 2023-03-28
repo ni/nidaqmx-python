c4c693e3575072adf8fbbd8fb129b492ba924389
Battu Mounika <battu.mounika@solitontech.com>
2023-03-28 12:44:17 +0530

    Updated logic to exclude parameters not used in nidaqmx-python

--- src/codegen/functions/function.py
+++ src/codegen/functions/function.py
@@ -26,7 +26,7 @@ class Function:
         if "parameters" in function_metadata:
             self._parameters = []
             for parameter in function_metadata["parameters"]:
-                if parameter["name"] != "task" and "python_data_type" in parameter:
+                if parameter["name"] != "task" and "python_data_type" in parameter and parameter.get("use_in_python_api") != False:
                     self._parameters.append(FunctionParameter(parameter))
 
                     if parameter["direction"] == "output":
