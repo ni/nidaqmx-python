868c3ccc645501caed925a023274b9f35154486f
Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>
2023-03-29 17:00:31 +0530

    fix: lint errors
    Signed-off-by: Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>

--- tests/test_persisted_scale_property.py
+++ tests/test_persisted_scale_property.py
@@ -10,14 +10,12 @@ class TestPersistedScaleProperty(TestDAQmxIOBase):
 
     def test_bool_property(self):
         """Test for validating bool attributes in persisted scale."""
-
         system = nidaqmx.system.System.local()
         persisted_scale = system.scales[0]
         assert persisted_scale.allow_interactive_editing
 
     def test_string_property(self):
         """Test for validating string attributes in persisted scale."""
-
         system = nidaqmx.system.System.local()
         persisted_scale = system.scales[0]
         persisted_scale.author
