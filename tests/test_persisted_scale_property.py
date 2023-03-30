674deb4ef4a27c753bb791aade8493dc5cb2beb2
Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>
2023-03-30 14:59:06 +0530

    feat: add fixture
    Signed-off-by: Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>

--- tests/test_persisted_scale_property.py
+++ tests/test_persisted_scale_property.py
@@ -1,4 +1,5 @@
 "Contains a collection of pytest tests that validates the persisted scale properties."
+import pytest
 
 import nidaqmx
 import nidaqmx.system.system
@@ -8,14 +9,12 @@ from nidaqmx.tests.test_read_write import TestDAQmxIOBase
 class TestPersistedScaleProperty(TestDAQmxIOBase):
     """Contains a collection of pytest tests that validates the persisted scale properties."""
 
-    def test_bool_property(self):
+    @pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect= True)
+    def test_bool_property(self, persisted_scale):
         """Test for validating bool attributes in persisted scale."""
-        system = nidaqmx.system.System.local()
-        persisted_scale = system.scales["double_gain_scale"]
         assert persisted_scale.allow_interactive_editing
 
-    def test_string_property(self):
+    @pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect= True)
+    def test_string_property(self, persisted_scale):
         """Test for validating string attributes in persisted scale."""
-        system = nidaqmx.system.System.local()
-        persisted_scale = system.scales["double_gain_scale"]
         assert persisted_scale.author == "Test Author"
