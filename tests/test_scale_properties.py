d82cb2a6dd3baaaea11328cde222f318a4811a87
Battu Mounika <battu.mounika@solitontech.com>
2023-03-31 00:23:46 +0530

    Updated persisted scale and followed conventions in tests

--- tests/test_scale_properties.py
+++ tests/test_scale_properties.py
@@ -1,66 +1,94 @@
-"Contains a collection of pytest tests that validates the scale properties."
+"Contains collection of pytest tests that validates the scale properties."
+
+import pytest
 
 from nidaqmx.constants import UnitsPreScaled
 from nidaqmx.errors import DaqError
 from nidaqmx.scale import Scale
 
 
-class TestScaleProperty(object):
-    """Contains a collection of pytest tests that validates the scale properties."""
+@pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect=True)
+def test__scale__float64_property__returns_default_value(persisted_scale):
+    """Test for validating getter for float property."""
+    scale = persisted_scale.load()
+    default_value = scale.lin_slope
 
-    def test_float64_property(self):
-        """Test for validating float property."""
-        scale = Scale("double_gain_scale")
+    assert scale.lin_slope == default_value
 
-        # Test property default value.
-        default_value = scale.lin_slope
-        assert scale.lin_slope == default_value
 
-        # Test property setter and getter.
-        scale.lin_slope = 5
-        assert scale.lin_slope == 5
+@pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect=True)
+def test__scale__set_float64_property__returns_assigned_value(persisted_scale):
+    """Test for validating setter for float property."""
+    scale = persisted_scale.load()
 
-    def test_list_float64_property(self):
-        """Test for validating float list property."""
-        scale = Scale("polynomial_scale")
+    scale.lin_slope = 5
 
-        # Test property default value.
-        default_value = scale.poly_forward_coeff
-        assert scale.poly_forward_coeff == default_value
+    assert scale.lin_slope == 5
 
-        # Test property setter and getter.
-        coeff_list = [1.0, 2.0]
-        scale.poly_forward_coeff = coeff_list
-        assert scale.poly_forward_coeff == coeff_list
 
-        # Test Incorrect property usage. Reading this property value throws DaqError.
-        linear_scale = Scale.create_lin_scale("custom_linear_scale", 5)
-        try:
-            _ = linear_scale.poly_forward_coeff
-        except DaqError as e:
-            assert e.error_code == -200601
+@pytest.mark.parametrize("persisted_scale", ["polynomial_scale"], indirect=True)
+def test__scale__float64_list_property__returns_default_value(persisted_scale):
+    """Test for validating getter for float list property."""
+    scale = persisted_scale.load()
+    default_value = scale.poly_forward_coeff
 
-    def test_enum_property(self):
-        """Test for validating enum property."""
-        scale = Scale.create_lin_scale(
-            "custom_linear_scale", 5, y_intercept=1, pre_scaled_units=UnitsPreScaled.VOLTS
-        )
+    assert scale.poly_forward_coeff == default_value
 
-        # Test property default value.
-        assert scale.pre_scaled_units == UnitsPreScaled.VOLTS
 
-        # Test property setter and getter.
-        scale.pre_scaled_units = UnitsPreScaled.AMPS
-        assert scale.pre_scaled_units == UnitsPreScaled.AMPS
+@pytest.mark.parametrize("persisted_scale", ["polynomial_scale"], indirect=True)
+def test__scale__set_float64_list_property__returns_assigned_value(persisted_scale):
+    """Test for validating setter for float list property."""
+    scale = persisted_scale.load()
 
-    def test_string_property(self):
-        """Test for validating string property."""
-        scale = Scale("double_gain_scale")
+    coeff_list = [1.0, 2.0]
+    scale.poly_forward_coeff = coeff_list
 
-        # Test property default value.
-        assert scale.description == ""
+    assert scale.poly_forward_coeff == coeff_list
 
-        # Test property setter and getter.
-        description = "Scale description."
-        scale.description = description
-        assert scale.description == description
+
+def test__scale__get_Invalid_float64_list_property__throws_DaqError():
+    """Test for validating getter for invalid float list property in linear scale."""
+    linear_scale = Scale.create_lin_scale("custom_linear_scale", 5)
+    try:
+        _ = linear_scale.poly_forward_coeff
+    except DaqError as e:
+        assert e.error_code == -200601
+
+
+def test__scale__enum_property__returns_default_value():
+    """Test for validating getter for enum property."""
+    scale = Scale.create_lin_scale(
+        "custom_linear_scale", 5, y_intercept=1, pre_scaled_units=UnitsPreScaled.VOLTS
+    )
+
+    assert scale.pre_scaled_units == UnitsPreScaled.VOLTS
+
+
+def test__scale__set_enum_property__returns_assigned_value():
+    """Test for validating setter for enum property."""
+    scale = Scale.create_lin_scale(
+        "custom_linear_scale", 5, y_intercept=1, pre_scaled_units=UnitsPreScaled.VOLTS
+    )
+
+    scale.pre_scaled_units = UnitsPreScaled.AMPS
+
+    assert scale.pre_scaled_units == UnitsPreScaled.AMPS
+
+
+@pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect=True)
+def test__scale__string_property__returns_default_value(persisted_scale):
+    """Test for validating getter for string property."""
+    scale = persisted_scale.load()
+
+    assert scale.description == ""
+
+
+@pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect=True)
+def test__scale__set_string_property__returns_assigned_value(persisted_scale):
+    """Test for validating setter for string property."""
+    scale = persisted_scale.load()
+
+    description = "Scale description."
+    scale.description = description
+
+    assert scale.description == description
