0c55383ca46e5fc9e7a808e5f9164cc106ec8dc1
Subash Suresh <subash.suresh@solitontech.com>
2023-03-31 20:05:23 +0530

    move the test class into component

--- tests/component/test_device_properties.py
+++ tests/component/test_device_properties.py
@@ -10,26 +10,31 @@ def test__device__bool_property__returns_value(device_by_name):
     # The PXIe-4331 supports analog triggering
     assert device_by_name.anlg_trig_supported
 
+
 @pytest.mark.parametrize("device_by_name", ["bridgeTester"], indirect=True)
 def test__device__float_property__returns_value(device_by_name):
     """Test for validating the device property of float type."""
     # The ai max single channel rate of the NI PXIe-4331 device is 102400.0
     assert device_by_name.ai_max_single_chan_rate == 102400.0
 
+
 @pytest.mark.parametrize("device_by_name", ["bridgeTester"], indirect=True)
 def test__device__uint_property__returns_value(device_by_name):
     """Test for validating the device property of uint type."""
     assert device_by_name.product_num == 0x74A9C4C4
+
 
 @pytest.mark.parametrize("device_by_name", ["bridgeTester"], indirect=True)
 def test__device__string_property__returns_value(device_by_name):
     """Test for validating a device property of string type."""
     assert device_by_name.product_type == "PXIe-4331"
 
+
 @pytest.mark.parametrize("device_by_name", ["bridgeTester"], indirect=True)
 def test__device__enum_property__returns_value(device_by_name):
     """Test for validating a device property of enum type."""
     assert device_by_name.bus_type == BusType.PXIE
+
 
 @pytest.mark.parametrize("device_by_name", ["bridgeTester"], indirect=True)
 def test__device__list_of_float_property__returns_value(device_by_name):
@@ -39,6 +44,7 @@ def test__device__list_of_float_property__returns_value(device_by_name):
     assert isinstance(ai_bridge_ranges, list)
     assert ai_bridge_ranges == [-0.025, 0.025, -0.1, 0.1]
 
+
 @pytest.mark.parametrize("device_by_name", ["bridgeTester"], indirect=True)
 def test__device__list_of_enum_property__returns_value(device_by_name):
     """Test for validating the device property of a enum array type."""
@@ -46,6 +52,7 @@ def test__device__list_of_enum_property__returns_value(device_by_name):
 
     assert isinstance(ai_trigger_usage, list)
     assert ai_trigger_usage == [TriggerUsage.REFERENCE, TriggerUsage.START]
+
 
 @pytest.mark.parametrize("device_by_name", ["bridgeTester"], indirect=True)
 def test__device__list_of_uint_property__returns_value(device_by_name):
