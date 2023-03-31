2e7dcaa7b3a19da47753995d9f52f65e4d370317
Subash Suresh <subash.suresh@solitontech.com>
2023-03-31 20:12:28 +0530

    update the then clause for the tests

--- tests/component/test_persisted_task_properties.py
+++ tests/component/test_persisted_task_properties.py
@@ -4,12 +4,12 @@ import pytest
 
 
 @pytest.mark.parametrize("persisted_task", ["VoltageTesterTask"], indirect=True)
-def test__persisted_task__get_bool_property__returns_value(persisted_task):
+def test__persisted_task__get_bool_property__returns_persisted_value(persisted_task):
     """Test for validating boolean properties in persisted task."""
     assert persisted_task.allow_interactive_editing
 
 
 @pytest.mark.parametrize("persisted_task", ["VoltageTesterTask"], indirect=True)
-def test__persisted_scale__get_string_property__returns_value(persisted_task):
+def test__persisted_scale__get_string_property__returns_persisted_value(persisted_task):
     """Test for validating string properties in persisted task."""
     assert persisted_task.author == "Test Author"
