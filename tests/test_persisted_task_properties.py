c10d713bb24367b6d9e95ba3ce71a8a212911264
Subash Suresh <subash.suresh@solitontech.com>
2023-03-31 12:44:17 +0530

    revamp to follow testing guidelines

--- tests/test_persisted_task_properties.py
+++ tests/test_persisted_task_properties.py
@@ -5,24 +5,28 @@ import nidaqmx
 import nidaqmx.system
 
 
-class TestPersistedTaskPropertyDataTypes(object):
-    """Contains a collection of pytest tests.
+@pytest.fixture(scope="module")
+def voltage_tester_task():
+    """Gets the voltage tester task from the persisted tasks in the system."""
+    system = nidaqmx.system.System.local()
 
-    This validates the property getter,setter and deleter methods for
-    persisted task properties for different data types.
-    """
-
-    @pytest.fixture(scope="class")
-    def voltage_tester_task(self):
-        """Gets the voltage tester task from the persisted tasks in the system."""
-        system = nidaqmx.system.System.local()
+    if "VoltageTesterTask" in system.tasks.task_names:
         voltage_tester_task = system.tasks["VoltageTesterTask"]
         return voltage_tester_task
 
-    def test_boolean_property(self, voltage_tester_task):
-        """Test for validating boolean properties in persisted task."""
-        assert voltage_tester_task.allow_interactive_editing
+    pytest.skip(
+        "Could not detect a persisted task named VoltageTesterTask. "
+        "Cannot proceed to run tests. Import the NI MAX configuration file located at "
+        "nidaqmx\\tests\\max_config\\nidaqmxMaxConfig.ini to create the required tasks."
+    )
+    return None
 
-    def test_string_property(self, voltage_tester_task):
-        """Test for validating string properties in persisted task."""
-        assert voltage_tester_task.author == "Test Author"
+
+def test__persisted_task__get_bool_property__returns_value(voltage_tester_task):
+    """Test for validating boolean properties in persisted task."""
+    assert voltage_tester_task.allow_interactive_editing
+
+
+def test__persisted_scale__get_string_property__returns_value(voltage_tester_task):
+    """Test for validating string properties in persisted task."""
+    assert voltage_tester_task.author == "Test Author"
