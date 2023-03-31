975c728dbd2d068cb0df5d8d63bf4cb118784daa
Subash Suresh <subash.suresh@solitontech.com>
2023-03-31 20:14:15 +0530

    update the then clause in tests

--- tests/component/test_persisted_channel_properties.py
+++ tests/component/test_persisted_channel_properties.py
@@ -3,12 +3,12 @@ import pytest
 
 
 @pytest.mark.parametrize("persisted_channel", ["VoltageTesterChannel"], indirect=True)
-def test__persisted_channel__get_bool_property__returns_value(persisted_channel):
+def test__persisted_channel__get_bool_property__returns_persisted_value(persisted_channel):
     """Test for validating boolean properties in persisted channel."""
     assert persisted_channel.allow_interactive_editing
 
 
 @pytest.mark.parametrize("persisted_channel", ["VoltageTesterChannel"], indirect=True)
-def test__persisted_channel__get_string_property__returns_value(persisted_channel):
+def test__persisted_channel__get_string_property__returns_persisted_value(persisted_channel):
     """Test for validating string properties in persisted channel."""
     assert persisted_channel.author == "Test Author"
