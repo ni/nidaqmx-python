6655cfe2f1f7a84a5eb87b11cb00da857c0443fd
Brad Keryan <brad.keryan@ni.com>
2023-03-31 18:11:03 -0500

    examples: Update nidaqmx_warnings to use X Series and STOPPED_BEFORE_DONE
    Use X Series to make the example easier to run from test_examples.py.
    
    Generate a STOPPED_BEFORE_DONE instead of POTENTIAL_GLITCH_DURING_WRITE
    to avoid intermittent failures. STOPPED_BEFORE_DONE is still timing-dependent,
    but it's more straightforward: stop the finite task before it's done and
    you get a warning.

--- nidaqmx_examples/nidaqmx_warnings.py
+++ nidaqmx_examples/nidaqmx_warnings.py
@@ -2,24 +2,25 @@
 import warnings
 
 import nidaqmx
+from nidaqmx.error_codes import DAQmxWarnings
 
 # By default warnings are shown.
 with nidaqmx.Task() as task:
-    task.ao_channels.add_ao_voltage_chan("cDAQ1Mod3/ao0")
+    task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
     task.timing.cfg_samp_clk_timing(1000)
 
     task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
-    task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
+    task.stop()
 
 
 # Catching warnings using the built-in warnings context manager.
 with nidaqmx.Task() as task:
-    task.ao_channels.add_ao_voltage_chan("cDAQ1Mod3/ao0")
+    task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
     task.timing.cfg_samp_clk_timing(1000)
 
     with warnings.catch_warnings(record=True) as w:
         task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
-        task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
+        task.stop()
 
         # Verify some things
         assert len(w) == 1
@@ -30,17 +31,17 @@ with nidaqmx.Task() as task:
 
 # Raising warnings as exceptions.
 with nidaqmx.Task() as task:
-    task.ao_channels.add_ao_voltage_chan("cDAQ1Mod3/ao0")
+    task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
     task.timing.cfg_samp_clk_timing(1000)
 
     warnings.filterwarnings("error", category=nidaqmx.DaqWarning)
 
     try:
         task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
-        task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
+        task.stop()
     except nidaqmx.DaqWarning as e:
         print("DaqWarning caught as exception: {0}\n".format(e))
-        assert e.error_code == 200015
+        assert e.error_code == DAQmxWarnings.STOPPED_BEFORE_DONE
 
 
 # Suppressing DaqWarnings.
@@ -48,8 +49,8 @@ print("Suppressing warnings.")
 warnings.filterwarnings("ignore", category=nidaqmx.DaqWarning)
 
 with nidaqmx.Task() as task:
-    task.ao_channels.add_ao_voltage_chan("cDAQ1Mod3/ao0")
+    task.ao_channels.add_ao_voltage_chan("Dev1/ao0")
     task.timing.cfg_samp_clk_timing(1000)
 
     task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
-    task.write([1.1, 2.2, 3.3, 4.4, 5.5], auto_start=True)
+    task.stop()
