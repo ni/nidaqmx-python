22cf9a7a4c1149ca3e346a9df799cade1b7f8e1d
Vikram Avudaiappan <vikram.avudaiappan@ni.com>
2023-04-02 13:52:40 +0530

    Updated tests with conventions and separating getter,setter, resetter

--- tests/test_channel_properties.py
+++ tests/test_channel_properties.py
@@ -1,197 +1,125 @@
-"""Tests for validating channel properties for different basic data types."""
-import random
+"""Contains a collection of pytest tests for validating channel properties for different basic data types."""
 
-import pytest
-
-import nidaqmx
-from nidaqmx.constants import (
-    BridgeConfiguration,
-    CounterFrequencyMethod,
-    Edge,
-    ExcitationSource,
-    FrequencyUnits,
-    PowerIdleOutputBehavior,
-    RTDType,
-    ResistanceConfiguration,
-    TemperatureUnits,
-    TerminalConfiguration,
-    TimeUnits,
-    VoltageUnits,
-)
-from nidaqmx.tests.helpers import generate_random_seed
+from nidaqmx._task_modules.channels.ai_channel import AIChannel
+from nidaqmx._task_modules.channels.ci_channel import CIChannel
+from nidaqmx.constants import PowerIdleOutputBehavior
 
 
-class TestChannelPropertyDataTypes(object):
-    """Contains a collection of pytest tests.
+def test__channel__get_boolean_property__returns_value(ai_voltage_chan_with_excit: AIChannel):
+    """Test to validate getter for channel property of boolean type."""
 
-    This validates the property getter,setter and deleter methods for different
-    data types of channel properties.
-    """
+    assert not ai_voltage_chan_with_excit.ai_excit_use_for_scaling
 
-    @pytest.mark.parametrize("seed", [generate_random_seed()])
-    def test_boolean_property(self, any_x_series_device, seed):
-        """Test for validating the channel property of boolean type."""
-        # Reset the pseudorandom number generator with seed.
-        random.seed(seed)
 
-        ai_phys_chan = random.choice(any_x_series_device.ai_physical_chans).name
+def test__channel__set_boolean_property__returns_assigned_value(
+    ai_voltage_chan_with_excit: AIChannel,
+):
+    """Test to validate setter for channel property of boolean type."""
 
-        with nidaqmx.Task() as task:
-            ai_channel = task.ai_channels.add_ai_voltage_chan_with_excit(
-                ai_phys_chan,
-                name_to_assign_to_channel="VoltageExcitChannel",
-                terminal_config=TerminalConfiguration.NRSE,
-                min_val=-10.0,
-                max_val=10.0,
-                units=VoltageUnits.VOLTS,
-                bridge_config=BridgeConfiguration.NO_BRIDGE,
-                voltage_excit_source=ExcitationSource.EXTERNAL,
-                voltage_excit_val=0.1,
-                use_excit_for_scaling=False,
-                custom_scale_name="",
-            )
+    ai_voltage_chan_with_excit.ai_excit_use_for_scaling = True
+    assert ai_voltage_chan_with_excit.ai_excit_use_for_scaling
 
-            # Test property initial value.
-            assert not ai_channel.ai_excit_use_for_scaling
 
-            # Test property setter and getter.
-            ai_channel.ai_excit_use_for_scaling = True
-            assert ai_channel.ai_excit_use_for_scaling
+def test__channel__reset_boolean_property__returns_initial_value(
+    ai_voltage_chan_with_excit: AIChannel,
+):
+    """Test to validate resetting channel property of boolean type."""
 
-            # Test property deleter.
-            del ai_channel.ai_excit_use_for_scaling
-            assert not ai_channel.ai_excit_use_for_scaling
+    del ai_voltage_chan_with_excit.ai_excit_use_for_scaling
+    assert not ai_voltage_chan_with_excit.ai_excit_use_for_scaling
 
-    @pytest.mark.parametrize("seed", [generate_random_seed()])
-    def test_enum_property(self, sim_ts_power_device, seed):
-        """Test for validating channel property of enum type."""
-        # Reset the pseudorandom number generator with seed.
-        random.seed(seed)
 
-        pwr_phys_chan = f"{sim_ts_power_device.name}/power"
-        voltage_setpoint = random.random() * 6.0
-        current_setpoint = random.random() * 3.0
-        output_enable = random.choice([True, False])
+def test__channel__get_enum_property__returns_value(ai_power_chan: AIChannel):
+    """Test to validate getter for channel property of enum type."""
 
-        with nidaqmx.Task() as task:
-            pwr_channel = task.ai_channels.add_ai_power_chan(
-                pwr_phys_chan,
-                voltage_setpoint,
-                current_setpoint,
-                output_enable,
-                name_to_assign_to_channel="PowerChannel",
-            )
+    # Default value is to maintain the existing value after the task is uncommitted
+    # in NI TS-15200 device
+    assert ai_power_chan.pwr_idle_output_behavior == PowerIdleOutputBehavior.MAINTAIN_EXISTING_VALUE
 
-            # Test property initial value.
-            assert (
-                pwr_channel.pwr_idle_output_behavior
-                == PowerIdleOutputBehavior.MAINTAIN_EXISTING_VALUE
-            )
 
-            # Test property setter and getter.
-            pwr_channel.pwr_idle_output_behavior = PowerIdleOutputBehavior.OUTPUT_DISABLED
-            assert pwr_channel.pwr_idle_output_behavior == PowerIdleOutputBehavior.OUTPUT_DISABLED
+def test__channel__set_enum_property__returns_assigned_value(ai_power_chan: AIChannel):
+    """Test to validate setter for channel property of enum type."""
 
-            # Test property deleter.
-            del pwr_channel.pwr_idle_output_behavior
-            assert (
-                pwr_channel.pwr_idle_output_behavior
-                == PowerIdleOutputBehavior.MAINTAIN_EXISTING_VALUE
-            )
+    ai_power_chan.pwr_idle_output_behavior = PowerIdleOutputBehavior.OUTPUT_DISABLED
+    assert ai_power_chan.pwr_idle_output_behavior == PowerIdleOutputBehavior.OUTPUT_DISABLED
 
-    @pytest.mark.parametrize("seed", [generate_random_seed()])
-    def test_float_property(self, any_x_series_device, seed):
-        """Test for validating channel property of float type."""
-        # Reset the pseudorandom number generator with seed.
-        random.seed(seed)
 
-        ai_phys_chan = random.choice(any_x_series_device.ai_physical_chans).name
+def test__channel__reset_enum_property__returns_initial_value(ai_power_chan: AIChannel):
+    """Test to validate resetting channel property of enum type."""
 
-        with nidaqmx.Task() as task:
-            ai_channel = task.ai_channels.add_ai_rtd_chan(
-                ai_phys_chan,
-                name_to_assign_to_channel="RTDChannel",
-                min_val=28.0,
-                max_val=120.0,
-                units=TemperatureUnits.K,
-                rtd_type=RTDType.PT_3750,
-                resistance_config=ResistanceConfiguration.TWO_WIRE,
-                current_excit_source=ExcitationSource.EXTERNAL,
-                current_excit_val=0.0025,
-                r_0=100.0,
-            )
+    # Default value is to maintain the existing value after the task is uncommitted
+    # in NI TS-15200 device
+    del ai_power_chan.pwr_idle_output_behavior
+    assert ai_power_chan.pwr_idle_output_behavior == PowerIdleOutputBehavior.MAINTAIN_EXISTING_VALUE
 
-            # Test property initial value.
-            assert ai_channel.ai_rtd_a == 0.00381
 
-            # Test property setter and getter.
-            value_to_test = random.random()
-            ai_channel.ai_rtd_a = value_to_test
-            assert ai_channel.ai_rtd_a == value_to_test
+def test__channel__get_float_property__returns_value(ai_rtd_chan: AIChannel):
+    """Test to validate getter for channel property of float type."""
 
-            # Test property deleter.
-            del ai_channel.ai_rtd_a
-            assert ai_channel.ai_rtd_a == 0.00381
+    # Default 'A' constant of the Callendar-Van Dusen equation in the NI PCIe-6363 device is "0.00381"
+    assert ai_rtd_chan.ai_rtd_a == 0.00381
 
-    @pytest.mark.parametrize("seed", [generate_random_seed()])
-    def test_string_property(self, any_x_series_device, seed):
-        """Test for validating channel property of string type."""
-        # Reset the pseudorandom number generator with seed.
-        random.seed(seed)
 
-        ci_phys_chan = random.choice(any_x_series_device.ci_physical_chans).name
+def test__channel__set_float_property__returns_assigned_value(ai_rtd_chan: AIChannel):
+    """Test to validate setter for channel property of float type."""
 
-        with nidaqmx.Task() as task:
-            ci_channel = task.ci_channels.add_ci_pulse_width_chan(
-                ci_phys_chan,
-                name_to_assign_to_channel="",
-                min_val=28.0,
-                max_val=120.0,
-                units=TimeUnits.SECONDS,
-                starting_edge=Edge.RISING,
-                custom_scale_name="",
-            )
+    value_to_set = 0.058388
+    ai_rtd_chan.ai_rtd_a = value_to_set
+    assert ai_rtd_chan.ai_rtd_a == value_to_set
 
-            # Test property initial value.
-            assert ci_channel.ci_ctr_timebase_dig_fltr_timebase_src == "100MHzTimebase"
 
-            # Test property setter and getter.
-            ci_channel.ci_ctr_timebase_dig_fltr_timebase_src = "20MHzTimebase"
-            assert ci_channel.ci_ctr_timebase_dig_fltr_timebase_src == "20MHzTimebase"
+def test__channel__reset_float_property__returns_initial_value(ai_rtd_chan: AIChannel):
+    """Test to validate resetting channel property of float type."""
 
-            # Test property deleter.
-            del ci_channel.ci_ctr_timebase_dig_fltr_timebase_src
-            assert ci_channel.ci_ctr_timebase_dig_fltr_timebase_src == "100MHzTimebase"
+    # Default 'A' constant of the Callendar-Van Dusen equation in the NI PCIe-6363 device is "0.00381"
+    del ai_rtd_chan.ai_rtd_a
+    assert ai_rtd_chan.ai_rtd_a == 0.00381
 
-    @pytest.mark.parametrize("seed", [generate_random_seed()])
-    def test_uint32_property(self, any_x_series_device, seed):
-        """Test for validating the channel property of uint32 type."""
-        # Reset the pseudorandom number generator with seed.
-        random.seed(seed)
 
-        ci_phys_chan = random.choice(any_x_series_device.ci_physical_chans).name
+def test__channel__get_string_property__returns_value(ci_pulse_width_chan: CIChannel):
+    """Test to validate getter for channel property of string type."""
 
-        with nidaqmx.Task() as task:
-            ci_channel = task.ci_channels.add_ci_freq_chan(
-                ci_phys_chan,
-                name_to_assign_to_channel="",
-                min_val=28.0,
-                max_val=120.0,
-                units=FrequencyUnits.HZ,
-                edge=Edge.RISING,
-                meas_method=CounterFrequencyMethod.LOW_FREQUENCY_1_COUNTER,
-                meas_time=0.01,
-                divisor=4,
-                custom_scale_name="",
-            )
+    # Default timebase value of the pulse width filter in the NI PCIe-6363 device is "100MHzTimebase"
+    assert ci_pulse_width_chan.ci_ctr_timebase_dig_fltr_timebase_src == "100MHzTimebase"
 
-            # Test property initial value.
-            assert ci_channel.ci_freq_div == 4
 
-            # Test property setter and getter.
-            ci_channel.ci_freq_div = 15
-            assert ci_channel.ci_freq_div == 15
+def test__channel__set_string_property__returns_assigned_value(ci_pulse_width_chan: CIChannel):
+    """Test to validate setter for channel property of string type."""
 
-            #  # Test property deleter.
-            del ci_channel.ci_freq_div
-            assert ci_channel.ci_freq_div == 4
+    value_to_set = "20MHzTimebase"
+    ci_pulse_width_chan.ci_ctr_timebase_dig_fltr_timebase_src = value_to_set
+    assert ci_pulse_width_chan.ci_ctr_timebase_dig_fltr_timebase_src == value_to_set
+
+
+def test__channel__reset_string_property__returns_initial_value(ci_pulse_width_chan: CIChannel):
+    """Test to validate resetting channel property of string type."""
+
+    # Default timebase value of the pulse width filter in the NI PCIe-6363 device is "100MHzTimebase"
+    del ci_pulse_width_chan.ci_ctr_timebase_dig_fltr_timebase_src
+    assert ci_pulse_width_chan.ci_ctr_timebase_dig_fltr_timebase_src == "100MHzTimebase"
+
+
+def test__channel__get_uint32_property__returns_value(ai_voltage_chan_with_excit: AIChannel):
+    """Test to validate getter for channel property of uint32 type."""
+
+    # Default number of bits to return in a raw sample in the NI PCIe-6363 device is "16"
+    assert ai_voltage_chan_with_excit.ai_lossy_lsb_removal_compressed_samp_size == 16
+
+
+def test__channel__set_uint32_property__returns_assigned_value(
+    ai_voltage_chan_with_excit: AIChannel,
+):
+    """Test to validate setter for channel property of uint32 type."""
+
+    ai_voltage_chan_with_excit.ai_lossy_lsb_removal_compressed_samp_size = 15
+    assert ai_voltage_chan_with_excit.ai_lossy_lsb_removal_compressed_samp_size == 15
+
+
+def test__channel__reset_uint32_property__returns_initial_value(
+    ai_voltage_chan_with_excit: AIChannel,
+):
+    """Test to validate resetting channel property of uint32 type."""
+
+    # Default number of bits to return in a raw sample in the NI PCIe-6363 device is "16"
+    del ai_voltage_chan_with_excit.ai_lossy_lsb_removal_compressed_samp_size
+    assert ai_voltage_chan_with_excit.ai_lossy_lsb_removal_compressed_samp_size == 16
