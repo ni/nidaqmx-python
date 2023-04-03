036dab809970e686945c282d8f789eeee0a6ffd0
Vikram Avudaiappan <vikram.avudaiappan@ni.com>
2023-04-04 00:48:28 +0530

    Fix lint errors

--- tests/conftest.py
+++ tests/conftest.py
@@ -203,6 +203,7 @@ def test_assets_directory() -> pathlib.Path:
     """Gets path to test_assets directory."""
     return pathlib.Path(__file__).parent / "test_assets"
 
+
 @pytest.fixture(scope="module")
 def ai_voltage_chan_task(any_x_series_device):
     """Gets AI Channel task."""
