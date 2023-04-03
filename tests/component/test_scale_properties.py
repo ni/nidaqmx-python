14d4a733cd1077bb6f3005d5823d21d8abb47cfe
Brad Keryan <brad.keryan@ni.com>
2023-04-03 16:12:35 -0500

    tests: Do not modify persisted scale properties
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>

--- tests/component/test_scale_properties.py
+++ tests/component/test_scale_properties.py
@@ -1,43 +1,41 @@
 "Contains collection of pytest tests that validates the scale properties."
 
-import pytest
-
 from nidaqmx.constants import UnitsPreScaled
 from nidaqmx.error_codes import DAQmxErrors
 from nidaqmx.errors import DaqError
 from nidaqmx.scale import Scale
 
+# Do not modify persisted scale properties in this test. Persisted scales are
+# cached for the lifetime of the process, so modifying persisted scale
+# properties will affect other tests that use the same scale.
 
-@pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect=True)
-def test__persisted_scale__get_float64_property__returns_persisted_value(persisted_scale):
+
+def test__scale__get_float64_property__returns_assigned_value():
     """Test for validating getter for float property."""
-    scale = persisted_scale.load()
+    scale = Scale.create_lin_scale("custom_linear_scale", 2)
 
     assert scale.lin_slope == 2.0
 
 
-@pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect=True)
-def test__persisted_scale__set_float64_property__returns_assigned_value(persisted_scale):
+def test__scale__set_float64_property__returns_assigned_value():
     """Test for validating setter for float property."""
-    scale = persisted_scale.load()
+    scale = Scale.create_lin_scale("custom_linear_scale", 2)
 
     scale.lin_slope = 5
 
-    assert scale.lin_slope == 5
+    assert scale.lin_slope == 5.0
 
 
-@pytest.mark.parametrize("persisted_scale", ["polynomial_scale"], indirect=True)
-def test__persisted_scale__get_float64_list_property__returns_persisted_value(persisted_scale):
+def test__scale__get_float64_list_property__returns_assigned_value():
     """Test for validating getter for float list property."""
-    scale = persisted_scale.load()
+    scale = Scale.create_polynomial_scale("custom_polynomial_scale", [0, 1], [0, 1])
 
     assert scale.poly_forward_coeff == [0.0, 1.0]
 
 
-@pytest.mark.parametrize("persisted_scale", ["polynomial_scale"], indirect=True)
-def test__persisted_scale__set_float64_list_property__returns_assigned_value(persisted_scale):
+def test__scale__set_float64_list_property__returns_assigned_value():
     """Test for validating setter for float list property."""
-    scale = persisted_scale.load()
+    scale = Scale.create_polynomial_scale("custom_polynomial_scale", [0, 1], [0, 1])
 
     coeff_list = [1.0, 2.0]
     scale.poly_forward_coeff = coeff_list
@@ -47,17 +45,17 @@ def test__persisted_scale__set_float64_list_property__returns_assigned_value(per
 
 def test__linear_scale__get_poly_scale_property__throws_daqerror():
     """Test for validating getter for polynomial scale float list property in linear scale."""
-    linear_scale = Scale.create_lin_scale("custom_linear_scale", 5)
+    linear_scale = Scale.create_lin_scale("custom_linear_scale", 1)
     try:
         _ = linear_scale.poly_forward_coeff
     except DaqError as e:
         assert e.error_type == DAQmxErrors.PROPERTY_NOT_SUPPORTED_FOR_SCALE_TYPE
 
 
-def test__scale__enum_property__returns_value():
+def test__scale__get_enum_property__returns_assigned_value():
     """Test for validating getter for enum property."""
     scale = Scale.create_lin_scale(
-        "custom_linear_scale", 5, y_intercept=1, pre_scaled_units=UnitsPreScaled.VOLTS
+        "custom_linear_scale", 1, y_intercept=1, pre_scaled_units=UnitsPreScaled.VOLTS
     )
 
     assert scale.pre_scaled_units == UnitsPreScaled.VOLTS
@@ -66,7 +64,7 @@ def test__scale__enum_property__returns_value():
 def test__scale__set_enum_property__returns_assigned_value():
     """Test for validating setter for enum property."""
     scale = Scale.create_lin_scale(
-        "custom_linear_scale", 5, y_intercept=1, pre_scaled_units=UnitsPreScaled.VOLTS
+        "custom_linear_scale", 1, y_intercept=1, pre_scaled_units=UnitsPreScaled.VOLTS
     )
 
     scale.pre_scaled_units = UnitsPreScaled.AMPS
@@ -74,25 +72,17 @@ def test__scale__set_enum_property__returns_assigned_value():
     assert scale.pre_scaled_units == UnitsPreScaled.AMPS
 
 
-def test__scale__string_property__returns_value():
+def test__scale__get_string_property__returns_assigned_value():
     """Test for validating getter for string property."""
-    scale = Scale.create_polynomial_scale(
-        "Custom_Polynominal_Scale",
-        [0, 1],
-        [0, 1],
-        pre_scaled_units=UnitsPreScaled.VOLTS,
-        scaled_units="AMPS",
-    )
+    scale = Scale.create_lin_scale("custom_linear_scale", 1, scaled_units="AMPS")
 
     assert scale.scaled_units == "AMPS"
 
 
-@pytest.mark.parametrize("persisted_scale", ["double_gain_scale"], indirect=True)
-def test__persisted_scale__set_string_property__returns_assigned_value(persisted_scale):
+def test__scale__set_string_property__returns_assigned_value():
     """Test for validating setter for string property."""
-    scale = persisted_scale.load()
+    scale = Scale.create_lin_scale("custom_linear_scale", 1, scaled_units="AMPS")
 
-    description = "Scale description."
-    scale.description = description
+    scale.scaled_units = "OHMS"
 
-    assert scale.description == description
+    assert scale.scaled_units == "OHMS"
