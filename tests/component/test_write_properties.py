c406a0ad8271c30cddb15acdddc408b0b008d411
Battu Mounika <battu.mounika@solitontech.com>
2023-04-04 16:13:06 +0530

    Fix: Commized fixture and updated device name

--- tests/component/test_write_properties.py
+++ tests/component/test_write_properties.py
@@ -6,135 +6,93 @@ import nidaqmx
 import nidaqmx.system
 from nidaqmx.constants import WriteRelativeTo
 from nidaqmx.errors import DaqError, DAQmxErrors
+from nidaqmx.task import Task
 
 
-def test__write__get_int32_property__returns_value(any_x_series_device):
+@pytest.fixture(scope="module")
+def write_task(any_x_series_device):
+    """Gets AO voltage task."""
+    with nidaqmx.Task() as task:
+        task.ao_channels.add_ao_voltage_chan(any_x_series_device.ao_physical_chans[0].name)
+        yield task
+
+
+def test__write__get_int32_property__returns_value(write_task: Task):
     """Test to validate getter for int32 property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-
-        assert write_task.out_stream.offset == 0
+    assert write_task.out_stream.offset == 0
 
 
-def test__write__set_int32_property__returns_assigned_value(any_x_series_device):
+def test__write__set_int32_property__returns_assigned_value(write_task: Task):
     """Test to validate setter for int32 property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
+    write_task.out_stream.offset = 1
 
-        write_task.out_stream.offset = 1
-
-        assert write_task.out_stream.offset == 1
+    assert write_task.out_stream.offset == 1
 
 
-def test__write__reset_int32_property__returns_default_value(any_x_series_device):
+def test__write__reset_int32_property__returns_default_value(write_task: Task):
     """Test to validate reset for int32 property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-        write_task.out_stream.offset = 2
+    write_task.out_stream.offset = 2
 
-        del write_task.out_stream.offset
+    del write_task.out_stream.offset
 
-        assert write_task.out_stream.offset == 0
+    assert write_task.out_stream.offset == 0
 
 
-def test__write__get_enum_property__returns_value(any_x_series_device):
+def test__write__get_enum_property__returns_value(write_task: Task):
     """Test to validate getter for enum property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-
-        assert write_task.out_stream.relative_to == WriteRelativeTo.CURRENT_WRITE_POSITION
+    assert write_task.out_stream.relative_to == WriteRelativeTo.CURRENT_WRITE_POSITION
 
 
-def test__write__set_enum_property__returns_assigned_value(any_x_series_device):
+def test__write__set_enum_property__returns_assigned_value(write_task: Task):
     """Test to validate setter for enum property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
+    write_task.out_stream.relative_to = WriteRelativeTo.FIRST_SAMPLE
 
-        write_task.out_stream.relative_to = WriteRelativeTo.FIRST_SAMPLE
-
-        assert write_task.out_stream.relative_to == WriteRelativeTo.FIRST_SAMPLE
+    assert write_task.out_stream.relative_to == WriteRelativeTo.FIRST_SAMPLE
 
 
-def test__write__reset_enum_property__returns_default_value(any_x_series_device):
+def test__write__reset_enum_property__returns_default_value(write_task: Task):
     """Test to validate reset for enum property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-        write_task.out_stream.relative_to = WriteRelativeTo.FIRST_SAMPLE
+    write_task.out_stream.relative_to = WriteRelativeTo.FIRST_SAMPLE
 
-        del write_task.out_stream.relative_to
+    del write_task.out_stream.relative_to
 
-        assert write_task.out_stream.relative_to == WriteRelativeTo.CURRENT_WRITE_POSITION
+    assert write_task.out_stream.relative_to == WriteRelativeTo.CURRENT_WRITE_POSITION
 
 
-def test__write__get_float_property__returns_value(any_x_series_device):
+def test__write__get_float_property__returns_value(write_task: Task):
     """Test to validate getter for float property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-
-        assert write_task.out_stream.sleep_time == 0.001
+    assert write_task.out_stream.sleep_time == 0.001
 
 
-def test__write__set_float_property__returns_assigned_value(any_x_series_device):
+def test__write__set_float_property__returns_assigned_value(write_task: Task):
     """Test to validate setter for float property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
+    write_task.out_stream.sleep_time = 1
 
-        write_task.out_stream.sleep_time = 1
-
-        assert write_task.out_stream.sleep_time == 1
+    assert write_task.out_stream.sleep_time == 1
 
 
-def test__write__reset_float_property__returns_default_value(any_x_series_device):
+def test__write__reset_float_property__returns_default_value(write_task: Task):
     """Test to validate reset for float property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-        write_task.out_stream.sleep_time = 3
+    write_task.out_stream.sleep_time = 3
 
-        del write_task.out_stream.sleep_time
+    del write_task.out_stream.sleep_time
 
-        assert write_task.out_stream.sleep_time == 0.001
+    assert write_task.out_stream.sleep_time == 0.001
 
 
-def test__write__get_uint32_property__returns_value(any_x_series_device):
+def test__write__get_uint32_property__returns_value(write_task: Task):
     """Test to validate getter for uInt32 property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-
-        assert write_task.out_stream.num_chans == 1
+    assert write_task.out_stream.num_chans == 1
 
 
-def test__write__get_uint64_property__returns_value(any_x_series_device):
+def test__write__get_uint64_property__returns_value(write_task: Task):
     """Test to validate getter for uInt64 property."""
-    with nidaqmx.Task() as write_task:
-        write_task.ao_channels.add_ao_voltage_chan(
-            any_x_series_device.ao_physical_chans[0].name, max_val=10, min_val=-10
-        )
-        write_task.start()
+    write_task.start()
         
-        assert write_task.out_stream.curr_write_pos == 0
+    assert write_task.out_stream.curr_write_pos == 0
 
 
-@pytest.mark.parametrize("device_by_name", ["Dev1"], indirect=True)
+@pytest.mark.parametrize("device_by_name", ["nidaqmxUSBTester"], indirect=True)
 def test__write__get_bool_property__returns_value(device_by_name):
     """Test to validate getter for bool property."""
     with nidaqmx.Task() as write_task:
@@ -144,7 +102,7 @@ def test__write__get_bool_property__returns_value(device_by_name):
         assert write_task.out_stream.open_current_loop_chans_exist
 
 
-@pytest.mark.parametrize("device_by_name", ["Dev1"], indirect=True)
+@pytest.mark.parametrize("device_by_name", ["nidaqmxUSBTester"], indirect=True)
 def test__write__get_string_property__returns_value(device_by_name):
     """Test to validate getter for string property."""
     with nidaqmx.Task() as write_task:
@@ -155,7 +113,7 @@ def test__write__get_string_property__returns_value(device_by_name):
         assert write_task.out_stream.power_supply_fault_chans == []
 
 
-@pytest.mark.parametrize("device_by_name", ["Dev1"], indirect=True)
+@pytest.mark.parametrize("device_by_name", ["nidaqmxUSBTester"], indirect=True)
 def test__write__read_property_out_of_order_throws_daqerror(device_by_name):
     """Test to validate error scenario for string property."""
     with nidaqmx.Task() as write_task:
