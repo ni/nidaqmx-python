058b96d81f01169144b6abca426ca339b0d9a70e
Battu Mounika <battu.mounika@solitontech.com>
2023-04-04 23:19:21 +0530

    Fix: Updated name read_task to ai_task

--- tests/component/test_read_properties.py
+++ tests/component/test_read_properties.py
@@ -9,156 +9,156 @@ from nidaqmx.task import Task
 
 
 @pytest.fixture()
-def read_task(any_x_series_device):
+def ai_task(any_x_series_device):
     """Gets AI voltage task."""
     with nidaqmx.Task() as task:
         task.ai_channels.add_ai_voltage_chan(any_x_series_device.ai_physical_chans[0].name)
         yield task
 
 
-def test__ai_task__get_int32_property__returns_default_value(read_task: Task):
+def test__ai_task__get_int32_property__returns_default_value(ai_task: Task):
     """Test to validate getter for int32 property."""
-    assert read_task.in_stream.offset == 0
+    assert ai_task.in_stream.offset == 0
 
 
-def test__ai_task__set_int32_property__returns_assigned_value(read_task: Task):
+def test__ai_task__set_int32_property__returns_assigned_value(ai_task: Task):
     """Test to validate setter for int32 property."""
-    read_task.in_stream.offset = 1
+    ai_task.in_stream.offset = 1
 
-    assert read_task.in_stream.offset == 1
+    assert ai_task.in_stream.offset == 1
 
 
-def test__ai_task__reset_int32_property__returns_default_value(read_task: Task):
+def test__ai_task__reset_int32_property__returns_default_value(ai_task: Task):
     """Test to validate reset for int32 property."""
-    read_task.in_stream.offset = 2
+    ai_task.in_stream.offset = 2
 
-    del read_task.in_stream.offset
+    del ai_task.in_stream.offset
 
-    assert read_task.in_stream.offset == 0
+    assert ai_task.in_stream.offset == 0
 
 
-def test__ai_task__get_enum_property__returns_default_value(read_task: Task):
+def test__ai_task__get_enum_property__returns_default_value(ai_task: Task):
     """Test to validate getter for enum property."""
-    assert read_task.in_stream.relative_to == ReadRelativeTo.CURRENT_READ_POSITION
+    assert ai_task.in_stream.relative_to == ReadRelativeTo.CURRENT_READ_POSITION
 
 
-def test__ai_task__set_enum_property__returns_assigned_value(read_task: Task):
+def test__ai_task__set_enum_property__returns_assigned_value(ai_task: Task):
     """Test to validate setter for enum property."""
-    read_task.in_stream.relative_to = ReadRelativeTo.FIRST_SAMPLE
+    ai_task.in_stream.relative_to = ReadRelativeTo.FIRST_SAMPLE
 
-    assert read_task.in_stream.relative_to == ReadRelativeTo.FIRST_SAMPLE
+    assert ai_task.in_stream.relative_to == ReadRelativeTo.FIRST_SAMPLE
 
 
-def test__ai_task__reset_enum_property__returns_default_value(read_task: Task):
+def test__ai_task__reset_enum_property__returns_default_value(ai_task: Task):
     """Test to validate reset for enum property."""
-    read_task.in_stream.relative_to = ReadRelativeTo.FIRST_SAMPLE
+    ai_task.in_stream.relative_to = ReadRelativeTo.FIRST_SAMPLE
 
-    del read_task.in_stream.relative_to
+    del ai_task.in_stream.relative_to
 
-    assert read_task.in_stream.relative_to == ReadRelativeTo.CURRENT_READ_POSITION
+    assert ai_task.in_stream.relative_to == ReadRelativeTo.CURRENT_READ_POSITION
 
 
-def test__ai_task__get_float_property__returns_default_value(read_task: Task):
+def test__ai_task__get_float_property__returns_default_value(ai_task: Task):
     """Test to validate getter for float property."""
-    assert read_task.in_stream.sleep_time == 0.001
+    assert ai_task.in_stream.sleep_time == 0.001
 
 
-def test__ai_task__set_float_property__returns_assigned_value(read_task: Task):
+def test__ai_task__set_float_property__returns_assigned_value(ai_task: Task):
     """Test to validate setter for float property."""
-    read_task.in_stream.sleep_time = 1.5
+    ai_task.in_stream.sleep_time = 1.5
 
-    assert read_task.in_stream.sleep_time == 1.5
+    assert ai_task.in_stream.sleep_time == 1.5
 
 
-def test__ai_task__reset_float_property__returns_default_value(read_task: Task):
+def test__ai_task__reset_float_property__returns_default_value(ai_task: Task):
     """Test to validate reset for float property."""
-    read_task.in_stream.sleep_time = 3.5
+    ai_task.in_stream.sleep_time = 3.5
 
-    del read_task.in_stream.sleep_time
+    del ai_task.in_stream.sleep_time
 
-    assert read_task.in_stream.sleep_time == 0.001
+    assert ai_task.in_stream.sleep_time == 0.001
 
 
-def test__ai_task__get_uint32_property__returns_default_value(read_task: Task):
+def test__ai_task__get_uint32_property__returns_default_value(ai_task: Task):
     """Test to validate getter for uInt32 property."""
-    assert read_task.in_stream.num_chans == 1
+    assert ai_task.in_stream.num_chans == 1
 
 
-def test__ai_task__set_uint32_property__returns_assigned_value(read_task: Task):
+def test__ai_task__set_uint32_property__returns_assigned_value(ai_task: Task):
     """Test to validate setter for uInt32 property."""
-    read_task.in_stream.logging_file_write_size = 50000
+    ai_task.in_stream.logging_file_write_size = 50000
 
-    assert read_task.in_stream.logging_file_write_size == 50000
+    assert ai_task.in_stream.logging_file_write_size == 50000
 
 
-def test__ai_task__reset_uint32_property__returns_default_value(read_task: Task):
+def test__ai_task__reset_uint32_property__returns_default_value(ai_task: Task):
     """Test to validate reset for uInt32 property."""
-    default_value = read_task.in_stream.logging_file_write_size
-    read_task.in_stream.logging_file_write_size = 30000
+    default_value = ai_task.in_stream.logging_file_write_size
+    ai_task.in_stream.logging_file_write_size = 30000
 
-    del read_task.in_stream.logging_file_write_size
+    del ai_task.in_stream.logging_file_write_size
 
-    assert read_task.in_stream.logging_file_write_size == default_value
+    assert ai_task.in_stream.logging_file_write_size == default_value
 
 
-def test__ai_task__get_uint64_property__returns_default_value(read_task: Task):
+def test__ai_task__get_uint64_property__returns_default_value(ai_task: Task):
     """Test to validate getter for uInt64 property."""
-    assert read_task.in_stream.logging_file_preallocation_size == 0
+    assert ai_task.in_stream.logging_file_preallocation_size == 0
 
 
-def test__ai_task__set_uint64_property__returns_assigned_value(read_task: Task):
+def test__ai_task__set_uint64_property__returns_assigned_value(ai_task: Task):
     """Test to validate setter for uInt64 property."""
-    read_task.in_stream.logging_file_preallocation_size = 100
+    ai_task.in_stream.logging_file_preallocation_size = 100
 
-    assert read_task.in_stream.logging_file_preallocation_size == 100
+    assert ai_task.in_stream.logging_file_preallocation_size == 100
 
 
-def test__ai_task__reset_uint64_property__returns_default_value(read_task: Task):
+def test__ai_task__reset_uint64_property__returns_default_value(ai_task: Task):
     """Test to validate reset for uInt64 property."""
-    read_task.in_stream.logging_file_preallocation_size = 100
+    ai_task.in_stream.logging_file_preallocation_size = 100
 
-    del read_task.in_stream.logging_file_preallocation_size
+    del ai_task.in_stream.logging_file_preallocation_size
 
-    assert read_task.in_stream.logging_file_preallocation_size == 0
+    assert ai_task.in_stream.logging_file_preallocation_size == 0
 
 
-def test__ai_task__get_bool_property__returns_default_value(read_task: Task):
+def test__ai_task__get_bool_property__returns_default_value(ai_task: Task):
     """Test to validate getter for bool property."""
-    assert not read_task.in_stream.logging_pause
+    assert not ai_task.in_stream.logging_pause
 
 
-def test__ai_task__set_bool_property__returns_assigned_value(read_task: Task):
+def test__ai_task__set_bool_property__returns_assigned_value(ai_task: Task):
     """Test to validate setter for bool property."""
-    read_task.in_stream.logging_pause = True
+    ai_task.in_stream.logging_pause = True
 
-    assert read_task.in_stream.logging_pause
+    assert ai_task.in_stream.logging_pause
 
 
-def test__ai_task__reset_bool_property__returns_default_value(read_task: Task):
+def test__ai_task__reset_bool_property__returns_default_value(ai_task: Task):
     """Test to validate reset for bool property."""
-    read_task.in_stream.logging_pause = True
+    ai_task.in_stream.logging_pause = True
 
-    del read_task.in_stream.logging_pause
+    del ai_task.in_stream.logging_pause
 
-    assert not read_task.in_stream.logging_pause
+    assert not ai_task.in_stream.logging_pause
 
 
-def test__ai_task__get_string_property__returns_default_value(read_task: Task):
+def test__ai_task__get_string_property__returns_default_value(ai_task: Task):
     """Test to validate getter for string property."""
-    assert read_task.in_stream.logging_file_path == ""
+    assert ai_task.in_stream.logging_file_path == ""
 
 
-def test__ai_task__set_string_property__returns_assigned_value(read_task: Task):
+def test__ai_task__set_string_property__returns_assigned_value(ai_task: Task):
     """Test to validate setter for string property."""
-    read_task.in_stream.logging_file_path = "TestData.tdms"
+    ai_task.in_stream.logging_file_path = "TestData.tdms"
 
-    assert read_task.in_stream.logging_file_path == "TestData.tdms"
+    assert ai_task.in_stream.logging_file_path == "TestData.tdms"
 
 
-def test__ai_task__reset_string_property__returns_default_value(read_task: Task):
+def test__ai_task__reset_string_property__returns_default_value(ai_task: Task):
     """Test to validate reset for string property."""
-    read_task.in_stream.logging_file_path = "TestData.tdms"
+    ai_task.in_stream.logging_file_path = "TestData.tdms"
 
-    del read_task.in_stream.logging_file_path
+    del ai_task.in_stream.logging_file_path
 
-    assert read_task.in_stream.logging_file_path == ""
+    assert ai_task.in_stream.logging_file_path == ""
