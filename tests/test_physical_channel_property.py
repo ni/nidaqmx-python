ca4e39fe726249e7ca040b800ae87af624acc42f
Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>
2023-04-04 18:00:54 +0530

    feat: add local fixtures
    Signed-off-by: Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>

--- tests/test_physical_channel_property.py
+++ tests/test_physical_channel_property.py
@@ -1,5 +1,6 @@
 "Contains a collection of pytest tests that validates the physical channel properties."
 import numpy
+import pytest
 
 from nidaqmx.constants import TerminalConfiguration
 
@@ -10,15 +11,14 @@ def test__physical_channel__get_bool_property__returns_value(any_x_series_device
     assert phys_chans[0].di_change_detect_supported
 
 
-def test__physical_channel__get_bit_stream__returns_assigned_value(
-    any_x_series_device, test_assets_directory
+def test__physical_channel_with_teds__get_bit_stream__returns_configured_value(
+    any_x_series_device, teds_file_path
 ):
     """Test for validating byte attributes in physical channel."""
     phys_chans = any_x_series_device.ai_physical_chans
     expected_value = numpy.array(VALUES_IN_TED, dtype=numpy.uint8)
 
     # Generate path to a virtual TEDS file.
-    teds_file_path = str(test_assets_directory / "teds" / "Voltage.ted")
     phys_chans["ai0"].configure_teds(teds_file_path)
 
     assert (phys_chans["ai0"].teds_bit_stream == expected_value).all()
@@ -36,43 +36,46 @@ def test__physical_channel__get_int32_array_property__returns_default_value(any_
     assert ai_channel.ai_term_cfgs == expected_configs
 
 
-def test__physical_channel__get_string_property__returns_assigned_value(
-    any_x_series_device, test_assets_directory
+def test__physical_channel_with_teds__get_string_property__returns_configured_value(
+    any_x_series_device, teds_file_path
 ):
     """Test for validating string attributes in physical channel."""
     phys_chans = any_x_series_device.ai_physical_chans
 
     # Generate path to a virtual TEDS file.
-    teds_file_path = str(test_assets_directory / "teds" / "Voltage.ted")
     phys_chans["ai0"].configure_teds(teds_file_path)
 
     assert phys_chans["ai0"].teds_version_letter == "A"
 
 
-def test__physical_channel__get_uint32_array_property__returns_assigned_value(
-    any_x_series_device, test_assets_directory
+def test__physical_channel_with_teds__get_uint32_array_property__returns_configured_value(
+    any_x_series_device, teds_file_path
 ):
     """Test for validating uint32 array attributes in physical channel."""
     phys_chans = any_x_series_device.ai_physical_chans
 
     # Generate path to a virtual TEDS file.
-    teds_file_path = str(test_assets_directory / "teds" / "Voltage.ted")
     phys_chans["ai0"].configure_teds(teds_file_path)
 
     assert phys_chans["ai0"].teds_template_ids == [30]
 
 
-def test__physical_channel__get_uint32_property__returns_assigned_value(
-    any_x_series_device, test_assets_directory
+def test__physical_channel_with_teds__get_uint32_property__returns_configured_value(
+    any_x_series_device, teds_file_path
 ):
     """Test for validating uint32 attributes in physical channel."""
     phys_chans = any_x_series_device.ai_physical_chans
 
     # Generate path to a virtual TEDS file.
-    teds_file_path = str(test_assets_directory / "teds" / "Voltage.ted")
     phys_chans["ai0"].configure_teds(teds_file_path)
 
     assert phys_chans["ai0"].teds_mfg_id == 17
+
+
+@pytest.fixture
+def teds_file_path(test_assets_directory):
+    """Returns the ted file path."""
+    return str(test_assets_directory / "teds" / "Voltage.ted")
 
 
 VALUES_IN_TED = [
