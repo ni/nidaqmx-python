e3d48676800a10a4b6264d899570365ae1d80315
Sakthi Subramanian Muthuramalingam <sakthisubramaniam@solitontech.com>
2023-04-04 22:30:27 +0530

    fix: remove unnecessary comments

--- tests/component/test_buffer_property.py
+++ tests/component/test_buffer_property.py
@@ -1,6 +1,10 @@
 "Contains a collection of pytest tests that validates the buffer property."
+import pytest
+
 import nidaqmx
 from nidaqmx.constants import SampleTimingType
+from nidaqmx.error_codes import DAQmxErrors
+from nidaqmx.errors import DaqError
 
 
 def test__ai_task__set_int32_property__value_is_set(any_x_series_device):
@@ -13,6 +17,20 @@ def test__ai_task__set_int32_property__value_is_set(any_x_series_device):
         task.in_stream.input_buf_size = 2000000000
 
         assert task.in_stream.input_buf_size == 2000000000
+
+
+def test__ai_task__set_valid_value_to_unsupported_property__unsupported_error_raised(
+    any_x_series_device,
+):
+    """Test for validating unsupported attribute in buffer."""
+    with nidaqmx.Task() as task:
+        task.ai_channels.add_ai_voltage_chan(any_x_series_device.ai_physical_chans[0].name)
+        task.timing.samp_timing_type = SampleTimingType.SAMPLE_CLOCK
+
+        with pytest.raises(DaqError) as exc_info:
+            task.out_stream.output_buf_size = 2000
+
+        assert exc_info.value.error_code == DAQmxErrors.ATTRIBUTE_NOT_SUPPORTED_IN_TASK_CONTEXT
 
 
 def test__ai_task__reset_int32_property__value_is_set_to_default(any_x_series_device):
