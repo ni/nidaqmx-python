d58b69d3fe20f091c7f1823f3e17b7d3da059c50
Battu Mounika <battu.mounika@solitontech.com>
2023-05-03 13:53:46 +0530

    Testing: Reverted back task.py changes

--- src/handwritten/task.py
+++ src/handwritten/task.py
@@ -77,7 +77,7 @@ class Task:
                 attempts to create multiple tasks with the same name, which
                 results in an error.
         """
-        self._handle = None
+        self._handle = lib_importer.task_handle(0)
 
         if grpc_options and not (
             grpc_options.session_name == "" or grpc_options.session_name == new_task_name
