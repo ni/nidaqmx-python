2e735bcfda95fc7b23a0be1d31640425df3f988b
Battu Mounika <battu.mounika@solitontech.com>
2023-05-08 12:25:36 +0530

    Cleanup: Moved & deprecated _lib dependent methods from errors.py to _library_interpreter.py

--- generated/nidaqmx/errors.py
+++ generated/nidaqmx/errors.py
@@ -1,5 +1,5 @@
-import ctypes
 import warnings
+import deprecation
 
 from nidaqmx.error_codes import DAQmxErrors, DAQmxWarnings
 
@@ -158,50 +158,19 @@ DaqResourceWarning = ResourceWarning
 warnings.filterwarnings("always", category=DaqWarning)
 warnings.filterwarnings("always", category=DaqResourceWarning)
 
-
+@deprecation.deprecated(deprecated_in="0.8.0", details="This function will be removed in a future update.")
 def check_for_error(error_code, samps_per_chan_written=None, samps_per_chan_read=None):
-    if not error_code:
-        return
-
-    from nidaqmx._lib import lib_importer
-
-    if error_code < 0:
-        error_buffer = ctypes.create_string_buffer(2048)
-
-        cfunc = lib_importer.windll.DAQmxGetExtendedErrorInfo
-        if cfunc.argtypes is None:
-            with cfunc.arglock:
-                if cfunc.argtypes is None:
-                    cfunc.argtypes = [ctypes.c_char_p, ctypes.c_uint]
-        cfunc(error_buffer, 2048)
-
-        if samps_per_chan_read is not None:
-            raise DaqReadError(error_buffer.value.decode("utf-8"), error_code, samps_per_chan_read)
-        elif samps_per_chan_written is not None:
-            raise DaqWriteError(error_buffer.value.decode("utf-8"), error_code, samps_per_chan_written)
-        else:
-            raise DaqError(error_buffer.value.decode("utf-8"), error_code)
-
-    elif error_code > 0:
-        error_buffer = ctypes.create_string_buffer(2048)
-
-        cfunc = lib_importer.windll.DAQmxGetErrorString
-        if cfunc.argtypes is None:
-            with cfunc.arglock:
-                if cfunc.argtypes is None:
-                    cfunc.argtypes = [ctypes.c_int, ctypes.c_char_p,
-                                      ctypes.c_uint]
-        cfunc(error_code, error_buffer, 2048)
-
-        warnings.warn(DaqWarning(
-            error_buffer.value.decode("utf-8"), error_code))
+    import nidaqmx._library_interpreter
+    return nidaqmx._library_interpreter.check_for_error(error_code, samps_per_chan_written, samps_per_chan_read)
 
 
+@deprecation.deprecated(deprecated_in="0.8.0", details="This function will be removed in a future update.")
 def is_string_buffer_too_small(error_code):
-    return (
-        error_code == DAQmxErrors.BUFFER_TOO_SMALL_FOR_STRING or
-        error_code == DAQmxWarnings.CAPI_STRING_TRUNCATED_TO_FIT_BUFFER)
+    import nidaqmx._library_interpreter
+    return nidaqmx._library_interpreter.is_string_buffer_too_small(error_code)
 
 
+@deprecation.deprecated(deprecated_in="0.8.0", details="This function will be removed in a future update.")
 def is_array_buffer_too_small(error_code):
-    return error_code == DAQmxErrors.WRITE_BUFFER_TOO_SMALL
+    import nidaqmx._library_interpreter
+    return nidaqmx._library_interpreter.is_array_buffer_too_small(error_code)
