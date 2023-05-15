33b7130137353d8665e82c93071940b980d40d28
Brad Keryan <brad.keryan@ni.com>
2023-05-15 11:17:39 -0500

    tests: Add tests for reader/writer dtype checks (#351)
    * tests: Add tests for reader/writer dtype checks
    
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>
    
    * tests: Use more accurate "then" clause for writer tests
    
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>
    
    ---------
    
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>

---
+++ tests/component/test_stream_writers.py
@@ -0,0 +1,82 @@
+import ctypes
+
+import numpy
+import pytest
+
+import nidaqmx
+from nidaqmx.stream_writers import AnalogMultiChannelWriter, AnalogSingleChannelWriter
+
+
+@pytest.fixture
+def ao_single_channel_task(
+    task: nidaqmx.Task, any_x_series_device: nidaqmx.system.Device
+) -> nidaqmx.Task:
+    task.ao_channels.add_ao_voltage_chan(any_x_series_device.ao_physical_chans[0].name)
+    return task
+
+
+@pytest.fixture
+def ao_multi_channel_task(
+    task: nidaqmx.Task, any_x_series_device: nidaqmx.system.Device
+) -> nidaqmx.Task:
+    task.ao_channels.add_ao_voltage_chan(any_x_series_device.ao_physical_chans[0].name)
+    task.ao_channels.add_ao_voltage_chan(any_x_series_device.ao_physical_chans[1].name)
+    return task
+
+
+def test___analog_single_channel_writer___write_many_sample___returns_samples_written(
+    ao_single_channel_task: nidaqmx.Task,
+):
+    writer = AnalogSingleChannelWriter(ao_single_channel_task.in_stream, auto_start=True)
+
+    samples_to_write = 10
+    data = numpy.full(samples_to_write, 1.234, dtype=numpy.float64)
+    samples_written = writer.write_many_sample(data)
+
+    assert samples_written == samples_to_write
+
+
+def test___analog_single_channel_writer___write_many_sample_with_wrong_dtype___raises_error_with_correct_dtype(
+    ao_single_channel_task: nidaqmx.Task,
+):
+    writer = AnalogSingleChannelWriter(ao_single_channel_task.in_stream, auto_start=True)
+
+    samples_to_write = 10
+    data = numpy.full(samples_to_write, 1.234, dtype=numpy.float32)
+    with pytest.raises((ctypes.ArgumentError, TypeError)) as exc_info:
+        _ = writer.write_many_sample(data)
+
+    assert "float64" in exc_info.value.args[0]
+
+
+@pytest.mark.xfail(
+    reason="AB#2393319: GrpcStubInterpreter writes empty array with AnalogMultiChannelWriter"
+)
+def test___analog_multi_channel_writer___write_many_sample___returns_samples_written(
+    ao_multi_channel_task: nidaqmx.Task,
+):
+    writer = AnalogMultiChannelWriter(ao_multi_channel_task.in_stream, auto_start=True)
+    num_channels = ao_multi_channel_task.number_of_channels
+    samples_to_write = 10
+
+    data = numpy.full((num_channels, samples_to_write), 1.234, dtype=numpy.float64)
+    samples_written = writer.write_many_sample(data)
+
+    assert samples_written == samples_to_write
+
+
+@pytest.mark.xfail(
+    reason="AB#2393319: GrpcStubInterpreter writes empty array with AnalogMultiChannelWriter"
+)
+def test___analog_multi_channel_writer___write_many_sample_with_wrong_dtype___raises_error_with_correct_dtype(
+    ao_multi_channel_task: nidaqmx.Task,
+):
+    writer = AnalogMultiChannelWriter(ao_multi_channel_task.in_stream, auto_start=True)
+    num_channels = ao_multi_channel_task.number_of_channels
+    samples_to_write = 10
+
+    data = numpy.full((num_channels, samples_to_write), 1.234, dtype=numpy.float32)
+    with pytest.raises((ctypes.ArgumentError, TypeError)) as exc_info:
+        _ = writer.write_many_sample(data)
+
+    assert "float64" in exc_info.value.args[0]
