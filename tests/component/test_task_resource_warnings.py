56fe6c65081f5c0cdbb366c47a0d71b436ec76ad
MounikaBattu17 <126869186+MounikaBattu17@users.noreply.github.com>
2023-05-23 11:54:23 +0530

    Disabled Resource warning for explicit detach of grpc task (#365)
    * Fix: Explicity disabled warnning for grpc task leaks
    
    * Tests: Update test cases to validate grpc task detach case

---
+++ tests/component/test_task_resource_warnings.py
@@ -0,0 +1,32 @@
+import warnings
+
+import pytest
+
+import nidaqmx
+from nidaqmx.errors import DaqResourceWarning
+
+
+@pytest.mark.library_only
+def test___unclosed_task___leak_task___resource_warning_raised(task):
+    # Since __del__ is not guaranteed to be called, for the purposes of
+    # consistent test results call __del__ manually.
+    with pytest.warns(DaqResourceWarning):
+        task.__del__()
+
+
+@pytest.mark.grpc_only
+def test___grpc_task___leak_task___resource_warning_not_raised(task):
+    with warnings.catch_warnings(record=True) as warnings_raised:
+        task.__del__()
+
+    assert len(warnings_raised) == 0
+
+
+def test___closed_task___del_task___resource_warning_not_raised(init_kwargs):
+    task = nidaqmx.Task(**init_kwargs)
+    task.close()
+
+    with warnings.catch_warnings(record=True) as warnings_raised:
+        task.__del__()
+
+    assert len(warnings_raised) == 0
--- tests/legacy/test_resource_warnings.py
+++ tests/legacy/test_resource_warnings.py
@@ -16,11 +16,3 @@ class TestResourceWarnings:
         with pytest.warns(DaqResourceWarning):
             with nidaqmx.Task(**init_kwargs) as task:
                 task.close()
-
-    def test_unclosed_task(self, init_kwargs):
-        """Test to validate unclosed tasks."""
-        task = nidaqmx.Task(**init_kwargs)
-        # Since __del__ is not guaranteed to be called, for the purposes of
-        # consistent test results call __del__ manually.
-        with pytest.warns(DaqResourceWarning):
-            task.__del__()
