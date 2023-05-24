53a7909c0ec2d1cd3c06b3d284b75ec1508f85e2
Brad Keryan <brad.keryan@ni.com>
2023-05-24 17:40:43 -0500

    tests: Add component tests for load_task() and markers to specify grpc_options (#376)
    * tests: Add marks to configure grpc_options
    
    Add grpc_session_name and grpc_session_initialization_behavior marks.
    
    Set scope=function for init_kwargs and system so that test
    functions can use marks to customize grpc_options.
    
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>
    
    * tests: Add component tests for load_task()
    
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>
    
    ---------
    
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>

--- tests/component/system/storage/test_persisted_task_properties.py
+++ tests/component/system/storage/test_persisted_task_properties.py
@@ -1,6 +1,11 @@
 import pytest
 
 from nidaqmx import DaqError
+from nidaqmx.constants import (
+    AcquisitionType,
+    TerminalConfiguration,
+    UsageTypeAI,
+)
 from nidaqmx.error_codes import DAQmxErrors
 from nidaqmx.system.storage import PersistedTask
 
@@ -23,10 +28,54 @@ def test___nonexistent_persisted_task___get_property___raises_task_not_in_data_n
 
 
 @pytest.mark.task_name("VoltageTesterTask")
-def test___persisted_task___get_bool_property___returns_persisted_value(persisted_task):
+def test___persisted_task___get_bool_property___returns_persisted_value(
+    persisted_task: PersistedTask,
+):
     assert persisted_task.allow_interactive_editing
 
 
 @pytest.mark.task_name("VoltageTesterTask")
-def test___persisted_scale___get_string_property___returns_persisted_value(persisted_task):
+def test___persisted_task___get_string_property___returns_persisted_value(
+    persisted_task: PersistedTask,
+):
     assert persisted_task.author == "Test Author"
+
+
+@pytest.mark.task_name("VoltageTesterTask")
+def test___persisted_task___load_and_get_channel_properties___returns_persisted_values(
+    persisted_task: PersistedTask,
+):
+    with persisted_task.load() as task:
+        ai_meas_type = task.ai_channels[0].ai_meas_type
+        ai_term_cfg = task.ai_channels[0].ai_term_cfg
+        ai_max = task.ai_channels[0].ai_max
+
+    assert ai_meas_type == UsageTypeAI.VOLTAGE
+    assert ai_term_cfg == TerminalConfiguration.DIFF
+    assert ai_max == 10.0
+
+
+@pytest.mark.task_name("VoltageTesterTask")
+def test___persisted_task___load_and_get_task_properties___returns_persisted_values(
+    persisted_task: PersistedTask,
+):
+    with persisted_task.load() as task:
+        channel_names = task.channel_names
+
+    assert channel_names == ["Voltage_0"]
+
+
+# Don't test samp_clk_src because it requires reserving the task and returns a coerced value,
+# not the persisted value.
+@pytest.mark.task_name("VoltageTesterTask")
+def test___persisted_task___load_and_get_timing_properties___returns_persisted_values(
+    persisted_task: PersistedTask,
+):
+    with persisted_task.load() as task:
+        samp_clk_rate = task.timing.samp_clk_rate
+        samp_quant_samp_mode = task.timing.samp_quant_samp_mode
+        samp_quant_samp_per_chan = task.timing.samp_quant_samp_per_chan
+
+    assert samp_clk_rate == 1000.0
+    assert samp_quant_samp_mode == AcquisitionType.FINITE
+    assert samp_quant_samp_per_chan == 100
