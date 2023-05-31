147d362a848f7dedb39457c7f170c0c7d76a61ec
Keerthana Kolandasamy <keerthana@solitontech.com>
2023-05-31 22:16:05 +0530

    fix: lint errors
    Signed-off-by: Keerthana Kolandasamy <keerthana@solitontech.com>

--- tests/component/system/test_system.py
+++ tests/component/system/test_system.py
@@ -7,6 +7,6 @@ def test___get_analog_power_up_states___returns_power_up_states(system):
     power_up_states = system.get_analog_power_up_states_with_output_type(channel_names)
 
     for i in range(len(channel_names)):
-        assert channel_names[i] ==  power_up_states[i].physical_channel
+        assert channel_names[i] == power_up_states[i].physical_channel
         assert 0.0 == power_up_states[i].power_up_state
         assert PowerUpChannelType.CHANNEL_HIGH_IMPEDANCE == power_up_states[i].channel_type
