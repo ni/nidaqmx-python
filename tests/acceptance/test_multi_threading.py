ea85ea20980fbfc61a9a4c73cce6f05b2af78f07
Brad Keryan <brad.keryan@ni.com>
2023-06-02 14:46:25 -0500

    tests: Remove xfail from multi-threaded event tests
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>

--- tests/acceptance/test_multi_threading.py
+++ tests/acceptance/test_multi_threading.py
@@ -173,10 +173,6 @@ def _release_n(semaphore: Semaphore, n: int) -> None:
         semaphore.release(n)
 
 
-@pytest.mark.grpc_xfail(
-    reason="Could not register the given callback function, a callback function already exists.",
-    raises=DaqError,
-)
 def test___shared_interpreter___run_multiple_acquisitions_with_events___callbacks_invoked(
     init_kwargs,
     multi_threading_test_devices: Sequence[Device],
@@ -205,11 +201,6 @@ def test___shared_interpreter___run_multiple_acquisitions_with_events___callback
         assert all(status == 0 for status in done_statuses)
 
 
-@pytest.mark.grpc_xfail(
-    reason="Could not register the given callback function, a callback function already exists.",
-    raises=DaqError,
-)
-@pytest.mark.library_xfail(reason="Crashes Python interpreter", run=False)
 def test___shared_interpreter___unregister_events_during_other_acquisitions_with_events___callbacks_invoked(
     init_kwargs,
     multi_threading_test_devices: Sequence[Device],
