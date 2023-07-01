baa0ccd805922484c2501c40e4619ded32c84866
Brad Keryan <brad.keryan@ni.com>
2023-07-01 15:57:17 -0500

    tests: Don't randomize the random seed (#431)
    Randomizing the random seed makes the GitHub test reporting action
    (EnricoMi/publish-unit-test-result-action) report many added/removed
    tests, so use the same random seed every time.
    
    Signed-off-by: Brad Keryan <brad.keryan@ni.com>

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,4 @@
 """This contains the helpers methods used in the DAQmx tests."""
-import random
-import sys
 
 
 # Power uses fixed-point scaling, so we have a pretty wide epsilon.
@@ -9,4 +7,7 @@ POWER_ABS_EPSILON = 1e-3
 
 def generate_random_seed():
     """Creates a random integer."""
-    return random.randint(0, sys.maxsize)
+    # Randomizing the random seed makes the GitHub test reporting action
+    # (EnricoMi/publish-unit-test-result-action) report many added/removed
+    # tests, so use the same random seed every time.
+    return 42
