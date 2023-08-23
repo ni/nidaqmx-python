143c6099cb796a2233b74eac7070e5932a7e5e5f
Zach Hindes <zach.hindes@ni.com>
2023-08-23 11:23:38 -0500

    remove global time imports for now

--- generated/nidaqmx/__init__.py
+++ generated/nidaqmx/__init__.py
@@ -2,7 +2,6 @@ from nidaqmx.errors import DaqError, DaqReadError, DaqWriteError, DaqWarning, Da
 from nidaqmx.grpc_session_options import *
 from nidaqmx.scale import Scale
 from nidaqmx.task import Task
-from nidaqmx.time import AbsoluteTime
 from nidaqmx.types import CtrFreq, CtrTick, CtrTime
 
 try:
