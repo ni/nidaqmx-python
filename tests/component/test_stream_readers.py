b2a316bb2c745d45ea4634493dedebe6ecf692d0
Zach Hindes <zach.hindes@ni.com>
2024-02-12 18:09:52 -0600

    code review

--- tests/component/test_stream_readers.py
+++ tests/component/test_stream_readers.py
@@ -1,6 +1,5 @@
 import ctypes
 import math
-from collections import namedtuple
 from typing import List
 
 import numpy
@@ -16,15 +15,15 @@ from nidaqmx.stream_readers import (
     PowerSingleChannelReader,
 )
 
-Limits = namedtuple("Limits", ["min_val", "max_val"])
+VOLTAGE_EPSILON = 1e-3
+VOLTAGE_BINARY_EPSILON = 4
 
 
 # Simulated DAQ voltage data is a noisy sinewave within the range of the minimum and maximum values
 # of the virtual channel. We can leverage this behavior to validate we get the correct data from
 # the Python bindings.
-def _get_voltage_limits_for_chan(chan_index: int) -> Limits:
-    #  max and min must be different, so we add a small offset to the max
-    return Limits(chan_index, chan_index + 0.01)
+def _get_voltage_offset_for_chan(chan_index: int) -> float:
+    return float(chan_index + 1)
 
 
 def _volts_to_codes(volts: float, max_code: int = 32767, max_voltage: float = 10.0) -> int:
@@ -32,20 +31,28 @@ def _volts_to_codes(volts: float, max_code: int = 32767, max_voltage: float = 10
 
 
 # Note: Since we only use positive voltages, this works fine for both signed and unsigned reads.
-def _get_voltage_code_limits_for_chan(chan_index: int) -> Limits:
-    voltage_limits = _get_voltage_limits_for_chan(chan_index)
-    return Limits(_volts_to_codes(voltage_limits.min_val), _volts_to_codes(voltage_limits.max_val))
+def _get_voltage_code_offset_for_chan(chan_index: int) -> int:
+    voltage_limits = _get_voltage_offset_for_chan(chan_index)
+    return _volts_to_codes(voltage_limits)
+
+
+def _validate_multi_channel_voltage_code_data(data: numpy.ndarray) -> None:
+    expected_vals = [
+        _get_voltage_code_offset_for_chan(chan_index) for chan_index in range(data.shape[0])
+    ]
+    assert numpy.min(data, axis=1) == pytest.approx(expected_vals, abs=VOLTAGE_BINARY_EPSILON)
+    assert numpy.max(data, axis=1) == pytest.approx(expected_vals, abs=VOLTAGE_BINARY_EPSILON)
 
 
 @pytest.fixture
 def ai_single_channel_task(
     task: nidaqmx.Task, sim_6363_device: nidaqmx.system.Device
 ) -> nidaqmx.Task:
-    min_val, max_val = _get_voltage_limits_for_chan(0)
+    offset = _get_voltage_offset_for_chan(0)
     task.ai_channels.add_ai_voltage_chan(
         sim_6363_device.ai_physical_chans[0].name,
-        min_val=min_val,
-        max_val=max_val,
+        min_val=offset,
+        max_val=offset + VOLTAGE_EPSILON,
     )
     return task
 
@@ -54,35 +61,17 @@ def ai_single_channel_task(
 def ai_multi_channel_task(
     task: nidaqmx.Task, sim_6363_device: nidaqmx.system.Device
 ) -> nidaqmx.Task:
-    min_val0, max_val0 = _get_voltage_limits_for_chan(0)
-    chan0 = task.ai_channels.add_ai_voltage_chan(
-        sim_6363_device.ai_physical_chans[0].name,
-        min_val=min_val0,
-        max_val=max_val0,
-    )
-    # forcing the maximum range for binary read scaling to be predictable
-    chan0.ai_rng_high = 10
-    chan0.ai_rng_low = -10
-
-    min_val1, max_val1 = _get_voltage_limits_for_chan(1)
-    chan1 = task.ai_channels.add_ai_voltage_chan(
-        sim_6363_device.ai_physical_chans[1].name,
-        min_val=min_val1,
-        max_val=max_val1,
-    )
-    # forcing the maximum range for binary read scaling to be predictable
-    chan1.ai_rng_high = 10
-    chan1.ai_rng_low = -10
-
-    min_val2, max_val2 = _get_voltage_limits_for_chan(2)
-    chan2 = task.ai_channels.add_ai_voltage_chan(
-        sim_6363_device.ai_physical_chans[2].name,
-        min_val=min_val2,
-        max_val=max_val2,
-    )
-    # forcing the maximum range for binary read scaling to be predictable
-    chan2.ai_rng_high = 10
-    chan2.ai_rng_low = -10
+    for chan_index in range(3):
+        offset = _get_voltage_offset_for_chan(chan_index)
+        chan = task.ai_channels.add_ai_voltage_chan(
+            sim_6363_device.ai_physical_chans[chan_index].name,
+            min_val=offset,
+            # min and max must be different, so add a small epsilon
+            max_val=offset + VOLTAGE_EPSILON,
+        )
+        # forcing the maximum range for binary read scaling to be predictable
+        chan.ai_rng_high = 10
+        chan.ai_rng_low = -10
 
     return task
 
@@ -94,8 +83,8 @@ def test___analog_single_channel_reader___read_one_sample___returns_valid_sample
 
     data = reader.read_one_sample()
 
-    limits = _get_voltage_limits_for_chan(0)
-    assert limits.min_val <= data and data <= limits.max_val
+    expected = _get_voltage_offset_for_chan(0)
+    assert data == pytest.approx(expected, abs=VOLTAGE_EPSILON)
 
 
 def test___analog_single_channel_reader___read_many_sample___returns_valid_samples(
@@ -108,8 +97,9 @@ def test___analog_single_channel_reader___read_many_sample___returns_valid_sampl
     samples_read = reader.read_many_sample(data, samples_to_read)
 
     assert samples_read == samples_to_read
-    limits = _get_voltage_limits_for_chan(0)
-    assert (limits.min_val <= data).all() and (data <= limits.max_val).all()
+    expected = _get_voltage_offset_for_chan(0)
+    assert numpy.min(data) == pytest.approx(expected, abs=VOLTAGE_EPSILON)
+    assert numpy.max(data) == pytest.approx(expected, abs=VOLTAGE_EPSILON)
 
 
 def test___analog_single_channel_reader___read_many_sample_with_wrong_dtype___raises_error_with_correct_dtype(
@@ -134,10 +124,8 @@ def test___analog_multi_channel_reader___read_one_sample___returns_valid_samples
 
     reader.read_one_sample(data)
 
-    for chan_index in range(num_channels):
-        limits = _get_voltage_limits_for_chan(chan_index)
-        chan_data = data[chan_index]
-        assert limits.min_val <= chan_data and chan_data <= limits.max_val
+    expected = [_get_voltage_offset_for_chan(chan_index) for chan_index in range(num_channels)]
+    assert data == pytest.approx(expected, abs=VOLTAGE_EPSILON)
 
 
 def test___analog_multi_channel_reader___read_one_sample_with_wrong_dtype___raises_error_with_correct_dtype(
@@ -164,10 +152,11 @@ def test___analog_multi_channel_reader___read_many_sample___returns_valid_sample
     samples_read = reader.read_many_sample(data, samples_to_read)
 
     assert samples_read == samples_to_read
-    for chan_index in range(num_channels):
-        limits = _get_voltage_limits_for_chan(chan_index)
-        chan_data = data[chan_index]
-        assert (limits.min_val <= chan_data).all() and (chan_data <= limits.max_val).all()
+    expected_vals = [
+        _get_voltage_offset_for_chan(chan_index) for chan_index in range(data.shape[0])
+    ]
+    assert numpy.min(data, axis=1) == pytest.approx(expected_vals, abs=VOLTAGE_EPSILON)
+    assert numpy.max(data, axis=1) == pytest.approx(expected_vals, abs=VOLTAGE_EPSILON)
 
 
 def test___analog_multi_channel_reader___read_many_sample_with_wrong_dtype___raises_error_with_correct_dtype(
@@ -197,10 +186,7 @@ def test___analog_unscaled_reader___read_int16___returns_valid_samples(
     samples_read = reader.read_int16(data, number_of_samples_per_channel=samples_to_read)
 
     assert samples_read == samples_to_read
-    for chan_index in range(num_channels):
-        limits = _get_voltage_code_limits_for_chan(chan_index)
-        chan_data = data[chan_index]
-        assert (limits.min_val <= chan_data).all() and (chan_data <= limits.max_val).all()
+    _validate_multi_channel_voltage_code_data(data)
 
 
 def test___analog_unscaled_reader___read_int16___raises_error_with_correct_dtype(
@@ -230,10 +216,7 @@ def test___analog_unscaled_reader___read_uint16___returns_valid_samples(
     samples_read = reader.read_uint16(data, number_of_samples_per_channel=samples_to_read)
 
     assert samples_read == samples_to_read
-    for chan_index in range(num_channels):
-        limits = _get_voltage_code_limits_for_chan(chan_index)
-        chan_data = data[chan_index]
-        assert (limits.min_val <= chan_data).all() and (chan_data <= limits.max_val).all()
+    _validate_multi_channel_voltage_code_data(data)
 
 
 def test___analog_unscaled_reader___read_uint16___raises_error_with_correct_dtype(
@@ -263,10 +246,7 @@ def test___analog_unscaled_reader___read_int32___returns_valid_samples(
     samples_read = reader.read_int32(data, number_of_samples_per_channel=samples_to_read)
 
     assert samples_read == samples_to_read
-    for chan_index in range(num_channels):
-        limits = _get_voltage_code_limits_for_chan(chan_index)
-        chan_data = data[chan_index]
-        assert (limits.min_val <= chan_data).all() and (chan_data <= limits.max_val).all()
+    _validate_multi_channel_voltage_code_data(data)
 
 
 def test___analog_unscaled_reader___read_int32___raises_error_with_correct_dtype(
@@ -296,10 +276,7 @@ def test___analog_unscaled_reader___read_uint32___returns_valid_samples(
     samples_read = reader.read_uint32(data, number_of_samples_per_channel=samples_to_read)
 
     assert samples_read == samples_to_read
-    for chan_index in range(num_channels):
-        limits = _get_voltage_code_limits_for_chan(chan_index)
-        chan_data = data[chan_index]
-        assert (limits.min_val <= chan_data).all() and (chan_data <= limits.max_val).all()
+    _validate_multi_channel_voltage_code_data(data)
 
 
 def test___analog_unscaled_reader___read_uint32___raises_error_with_correct_dtype(
@@ -316,16 +293,16 @@ def test___analog_unscaled_reader___read_uint32___raises_error_with_correct_dtyp
     assert "uint32" in exc_info.value.args[0]
 
 
-PowerSetpoint = namedtuple("PowerSetpoint", ["voltage", "current"])
-# The minimum current setpoint for our TestScale device is 3mA
-POWER_CURRENT_MINIMUM = 0.03
 POWER_EPSILON = 1e-3
 POWER_BINARY_EPSILON = 1
 
 
-# Simulated DAQ power data is exactly the setpoints, if enabled.
-def _get_power_setpoints_for_chan(chan_index: int) -> Limits:
-    return PowerSetpoint(chan_index, max(chan_index, POWER_CURRENT_MINIMUM))
+def _get_voltage_setpoint_for_chan(chan_index: int) -> float:
+    return float(chan_index + 1)
+
+
+def _get_current_setpoint_for_chan(chan_index: int) -> float:
+    return float(chan_index + 1)
 
 
 def _pwr_volts_to_codes(volts: float, codes_per_volt: int = 4096) -> int:
@@ -336,22 +313,22 @@ def _pwr_current_to_codes(current: float, codes_per_amp: int = 8192) -> int:
     return int(current * codes_per_amp)
 
 
-def _get_power_code_setpoints_for_chan(chan_index: int) -> Limits:
-    power_limits = _get_power_setpoints_for_chan(chan_index)
-    return PowerSetpoint(
-        _pwr_volts_to_codes(power_limits.voltage), _pwr_current_to_codes(power_limits.current)
-    )
+def _get_voltage_code_setpoint_for_chan(chan_index: int) -> int:
+    return _pwr_volts_to_codes(_get_voltage_setpoint_for_chan(chan_index))
+
+
+def _get_current_code_setpoint_for_chan(chan_index: int) -> int:
+    return _pwr_current_to_codes(_get_current_setpoint_for_chan(chan_index))
 
 
 @pytest.fixture
 def pwr_single_channel_task(
     task: nidaqmx.Task, sim_ts_power_device: nidaqmx.system.Device
 ) -> nidaqmx.Task:
-    voltage_setpoint, current_setpoint = _get_power_setpoints_for_chan(0)
     task.ai_channels.add_ai_power_chan(
         f"{sim_ts_power_device.name}/power",
-        voltage_setpoint,
-        current_setpoint,
+        _get_voltage_setpoint_for_chan(0),
+        _get_current_setpoint_for_chan(0),
         True,  # output enable
     )
     return task
@@ -362,11 +339,10 @@ def pwr_multi_channel_task(
     task: nidaqmx.Task, sim_ts_power_devices: List[nidaqmx.system.Device]
 ) -> nidaqmx.Task:
     for chan_index, sim_ts_power_device in enumerate(sim_ts_power_devices):
-        voltage_setpoint, current_setpoint = _get_power_setpoints_for_chan(chan_index)
         task.ai_channels.add_ai_power_chan(
             f"{sim_ts_power_device.name}/power",
-            voltage_setpoint,
-            current_setpoint,
+            _get_voltage_setpoint_for_chan(chan_index),
+            _get_current_setpoint_for_chan(chan_index),
             True,  # output enable
         )
     return task
@@ -379,9 +355,8 @@ def test___power_single_channel_reader___read_one_sample___returns_valid_samples
 
     data = reader.read_one_sample()
 
-    setpoints = _get_power_setpoints_for_chan(0)
-    assert data.voltage == pytest.approx(setpoints.voltage, abs=POWER_EPSILON)
-    assert data.current == pytest.approx(setpoints.current, abs=POWER_EPSILON)
+    assert data.voltage == pytest.approx(_get_voltage_setpoint_for_chan(0), abs=POWER_EPSILON)
+    assert data.current == pytest.approx(_get_current_setpoint_for_chan(0), abs=POWER_EPSILON)
 
 
 def test___power_single_channel_reader___read_many_sample___returns_valid_samples(
@@ -395,9 +370,8 @@ def test___power_single_channel_reader___read_many_sample___returns_valid_sample
     samples_read = reader.read_many_sample(voltage_data, current_data, samples_to_read)
 
     assert samples_read == samples_to_read
-    setpoints = _get_power_setpoints_for_chan(0)
-    assert voltage_data == pytest.approx(setpoints.voltage, abs=POWER_EPSILON)
-    assert current_data == pytest.approx(setpoints.current, abs=POWER_EPSILON)
+    assert voltage_data == pytest.approx(_get_voltage_setpoint_for_chan(0), abs=POWER_EPSILON)
+    assert current_data == pytest.approx(_get_current_setpoint_for_chan(0), abs=POWER_EPSILON)
 
 
 def test___power_single_channel_reader___read_many_sample_with_wrong_dtype___raises_error_with_correct_dtype(
@@ -429,10 +403,14 @@ def test___power_multi_channel_reader___read_one_sample___returns_valid_samples(
 
     reader.read_one_sample(voltage_data, current_data)
 
-    for chan_index in range(num_channels):
-        setpoints = _get_power_setpoints_for_chan(chan_index)
-        assert voltage_data[chan_index] == pytest.approx(setpoints.voltage, abs=POWER_EPSILON)
-        assert current_data[chan_index] == pytest.approx(setpoints.current, abs=POWER_EPSILON)
+    assert voltage_data == pytest.approx(
+        [_get_voltage_setpoint_for_chan(chan_index) for chan_index in range(num_channels)],
+        abs=POWER_EPSILON,
+    )
+    assert current_data == pytest.approx(
+        [_get_current_setpoint_for_chan(chan_index) for chan_index in range(num_channels)],
+        abs=POWER_EPSILON,
+    )
 
 
 def test___power_multi_channel_reader___read_one_sample_with_wrong_dtype___raises_error_with_correct_dtype(
@@ -468,10 +446,24 @@ def test___power_multi_channel_reader___read_many_sample___returns_valid_samples
     )
 
     assert samples_read == samples_to_read
-    for chan_index in range(num_channels):
-        setpoints = _get_power_setpoints_for_chan(chan_index)
-        assert voltage_data[chan_index] == pytest.approx(setpoints.voltage, abs=POWER_EPSILON)
-        assert current_data[chan_index] == pytest.approx(setpoints.current, abs=POWER_EPSILON)
+    expected_voltage_vals = [
+        _get_voltage_setpoint_for_chan(chan_index) for chan_index in range(num_channels)
+    ]
+    expected_current_vals = [
+        _get_current_setpoint_for_chan(chan_index) for chan_index in range(num_channels)
+    ]
+    assert numpy.min(voltage_data, axis=1) == pytest.approx(
+        expected_voltage_vals, abs=POWER_EPSILON
+    )
+    assert numpy.max(voltage_data, axis=1) == pytest.approx(
+        expected_voltage_vals, abs=POWER_EPSILON
+    )
+    assert numpy.min(current_data, axis=1) == pytest.approx(
+        expected_current_vals, abs=POWER_EPSILON
+    )
+    assert numpy.max(current_data, axis=1) == pytest.approx(
+        expected_current_vals, abs=POWER_EPSILON
+    )
 
 
 def test___power_multi_channel_reader___read_many_sample_with_wrong_dtype___raises_error_with_correct_dtype(
@@ -516,14 +508,24 @@ def test___power_binary_reader___read_many_sample___returns_valid_samples(
     )
 
     assert samples_read == samples_to_read
-    for chan_index in range(num_channels):
-        setpoints = _get_power_code_setpoints_for_chan(chan_index)
-        assert voltage_data[chan_index] == pytest.approx(
-            setpoints.voltage, abs=POWER_BINARY_EPSILON
-        )
-        assert current_data[chan_index] == pytest.approx(
-            setpoints.current, abs=POWER_BINARY_EPSILON
-        )
+    expected_voltage_vals = [
+        _get_voltage_code_setpoint_for_chan(chan_index) for chan_index in range(num_channels)
+    ]
+    expected_current_vals = [
+        _get_current_code_setpoint_for_chan(chan_index) for chan_index in range(num_channels)
+    ]
+    assert numpy.min(voltage_data, axis=1) == pytest.approx(
+        expected_voltage_vals, abs=POWER_BINARY_EPSILON
+    )
+    assert numpy.max(voltage_data, axis=1) == pytest.approx(
+        expected_voltage_vals, abs=POWER_BINARY_EPSILON
+    )
+    assert numpy.min(current_data, axis=1) == pytest.approx(
+        expected_current_vals, abs=POWER_BINARY_EPSILON
+    )
+    assert numpy.max(current_data, axis=1) == pytest.approx(
+        expected_current_vals, abs=POWER_BINARY_EPSILON
+    )
 
 
 def test___power_binary_reader___read_many_sample_with_wrong_dtype___raises_error_with_correct_dtype(
