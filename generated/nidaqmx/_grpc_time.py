a0b3a21750d5af3c7554421f02e8e9060f5cfcd5
DeborahOoi96 <deborahooi96@gmail.com>
2024-02-20 14:31:57 +0800

    Update libtime to accept time before 1970

--- generated/nidaqmx/_grpc_time.py
+++ generated/nidaqmx/_grpc_time.py
@@ -1,10 +1,13 @@
 from datetime import timezone
 from datetime import datetime as std_datetime
 from hightime import datetime as ht_datetime
+from hightime import timedelta as ht_timedelta
 from typing import Optional, Union
 
 from google.protobuf.timestamp_pb2 import Timestamp as GrpcTimestamp
 
+# 66 years, 17 leap days = 24107 days = 2082844800 seconds
+_BIAS_FROM_1970_EPOCH = 2082844800
 
 _NS_PER_S = 10**9
 _NS_PER_US = 10**3
@@ -35,19 +38,38 @@ def convert_time_to_timestamp(dt: Union[std_datetime, ht_datetime], ts: Optional
     return ts
 
 
+def convert_to_desired_timezone(expected_time_utc, tzinfo):
+    current_time_utc = ht_datetime.now(timezone.utc)
+    desired_timezone_offset = current_time_utc.astimezone(tz=tzinfo).utcoffset()
+    desired_expected_time = expected_time_utc + desired_timezone_offset
+    new_datetime = ht_datetime(
+            desired_expected_time.year,
+            desired_expected_time.month,
+            desired_expected_time.day,
+            desired_expected_time.hour,
+            desired_expected_time.minute,
+            desired_expected_time.second,
+            desired_expected_time.microsecond,
+            desired_expected_time.femtosecond,
+            desired_expected_time.yoctosecond,
+            tzinfo = tzinfo)
+    return new_datetime
+
+
+def timestamp_to_1904_epoch(timestamp, yoctoseconds):
+    epoch_1904 = ht_datetime(1904, 1, 1)
+    if timestamp < 0:
+        seconds_from_1904 = ht_timedelta(seconds = _BIAS_FROM_1970_EPOCH - abs(timestamp))
+    else:
+        seconds_from_1904 = ht_timedelta(seconds = _BIAS_FROM_1970_EPOCH + abs(timestamp))
+    datetime_1904 = epoch_1904 + seconds_from_1904 + ht_timedelta(yoctoseconds=yoctoseconds)
+    return datetime_1904
+
+
 def convert_timestamp_to_time(ts: GrpcTimestamp, tzinfo: Optional[timezone] = None) -> ht_datetime:
     total_nanos = ts.ToNanoseconds()
     seconds, nanos = divmod(total_nanos, _NS_PER_S)
-
-    # Convert the nanoseconds to micro, femto, and yoctorseconds.
+    # Convert the nanoseconds to yoctoseconds.
     total_yoctoseconds = int(round(_YS_PER_NS * nanos))
-    microsecond, remainder_yoctoseconds = divmod(total_yoctoseconds, _YS_PER_US)
-    femtosecond, remainder_yoctoseconds = divmod(remainder_yoctoseconds, _YS_PER_FS)
-    yoctosecond = remainder_yoctoseconds
-
-    # Start with UTC
-    dt = ht_datetime.fromtimestamp(seconds, timezone.utc)
-    # Add in precision
-    dt = dt.replace(microsecond=microsecond, femtosecond=femtosecond, yoctosecond=yoctosecond)
-    # Then convert to requested timezone
-    return dt.astimezone(tz=tzinfo)
+    dt = timestamp_to_1904_epoch(seconds, total_yoctoseconds)
+    return convert_to_desired_timezone(dt, tzinfo)
