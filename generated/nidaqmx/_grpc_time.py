bf47e9c958e57b9371b3d6cfa68c4730013477bc
DeborahOoi96 <deborahooi96@gmail.com>
2024-02-26 14:44:40 +0800

    Try astimezone

--- generated/nidaqmx/_grpc_time.py
+++ generated/nidaqmx/_grpc_time.py
@@ -1,10 +1,13 @@
 from datetime import timezone
 from datetime import datetime as std_datetime
 from hightime import datetime as ht_datetime
+from hightime import timedelta as ht_timedelta
 from typing import Optional, Union
 
 from google.protobuf.timestamp_pb2 import Timestamp as GrpcTimestamp
 
+# 66 years, 17 leap days = 24107 days = 2082844800 seconds
+_BIAS_FROM_1970_EPOCH = 2082844800
 
 _NS_PER_S = 10**9
 _NS_PER_US = 10**3
@@ -13,10 +16,31 @@ _YS_PER_US = 10**18
 _YS_PER_NS = 10**15
 _YS_PER_FS = 10**9
 
+_EPOCH_1970 = ht_datetime(1970, 1, 1, tzinfo=timezone.utc)
+
+def _convert_to_desired_timezone(expected_time_utc: Union[std_datetime, ht_datetime], tzinfo: Optional[timezone] = None):
+    current_time_utc = ht_datetime.now(timezone.utc)
+    desired_timezone_offset = current_time_utc.astimezone(tz=tzinfo).utcoffset()
+    desired_expected_time = expected_time_utc + desired_timezone_offset
+    new_datetime = ht_datetime(
+        desired_expected_time.year,
+        desired_expected_time.month,
+        desired_expected_time.day,
+        desired_expected_time.hour,
+        desired_expected_time.minute,
+        desired_expected_time.second,
+        desired_expected_time.microsecond,
+        desired_expected_time.femtosecond,
+        desired_expected_time.yoctosecond,
+        tzinfo = tzinfo)
+    return new_datetime
+
 
 def convert_time_to_timestamp(dt: Union[std_datetime, ht_datetime], ts: Optional[GrpcTimestamp] = None) -> GrpcTimestamp:
-    utc_dt = dt.astimezone(tz=timezone.utc)
-    seconds = int(utc_dt.timestamp())
+    seconds_since_1970 = int((dt - _EPOCH_1970).total_seconds())
+    # We need to add one more negative second if applicable to compensate for a non-zero microsecond.
+    if dt.microsecond and (dt < _EPOCH_1970):
+        seconds_since_1970 -=1
     if ts is None:
         ts = GrpcTimestamp()
 
@@ -29,25 +53,15 @@ def convert_time_to_timestamp(dt: Union[std_datetime, ht_datetime], ts: Optional
         if remainder_yoctoseconds >= _YS_PER_NS / 2:
             nanos += 1
     else:
-        nanos = utc_dt.microsecond * _NS_PER_US
+        nanos = dt.microsecond * _NS_PER_US
 
-    ts.FromNanoseconds(seconds * _NS_PER_S + nanos)
+    ts.FromNanoseconds(seconds_since_1970 * _NS_PER_S + nanos)
     return ts
-
 
 def convert_timestamp_to_time(ts: GrpcTimestamp, tzinfo: Optional[timezone] = None) -> ht_datetime:
     total_nanos = ts.ToNanoseconds()
     seconds, nanos = divmod(total_nanos, _NS_PER_S)
-
-    # Convert the nanoseconds to micro, femto, and yoctorseconds.
+    # Convert the nanoseconds to yoctoseconds.
     total_yoctoseconds = int(round(_YS_PER_NS * nanos))
-    microsecond, remainder_yoctoseconds = divmod(total_yoctoseconds, _YS_PER_US)
-    femtosecond, remainder_yoctoseconds = divmod(remainder_yoctoseconds, _YS_PER_FS)
-    yoctosecond = remainder_yoctoseconds
-
-    # Start with UTC
-    dt = ht_datetime.fromtimestamp(seconds, timezone.utc)
-    # Add in precision
-    dt = dt.replace(microsecond=microsecond, femtosecond=femtosecond, yoctosecond=yoctosecond)
-    # Then convert to requested timezone
+    dt = _EPOCH_1970 + ht_timedelta(seconds = seconds) + ht_timedelta(yoctoseconds=total_yoctoseconds)
     return dt.astimezone(tz=tzinfo)
