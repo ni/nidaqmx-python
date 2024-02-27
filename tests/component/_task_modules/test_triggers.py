22ae4551e3ec9cee85a3a4fe239d2752672f62cd
DeborahOoi96 <74756143+DeborahOoi96@users.noreply.github.com>
2024-02-26 19:55:36 -0600

    Change ai_voltage_field_daq_task to ai_voltage_task (#520)

--- tests/component/_task_modules/test_triggers.py
+++ tests/component/_task_modules/test_triggers.py
@@ -8,22 +8,22 @@ from nidaqmx.task import Task
 
 
 @pytest.fixture()
-def ai_voltage_field_daq_task(task, sim_time_aware_9215_device):
+def ai_voltage_task(task, sim_time_aware_9215_device):
     """Gets AI voltage task."""
     task.ai_channels.add_ai_voltage_chan(sim_time_aware_9215_device.ai_physical_chans[0].name)
     yield task
 
 
 def test___default_arguments___cfg_time_start_trig___no_errors(
-    ai_voltage_field_daq_task: Task,
+    ai_voltage_task: Task,
 ):
-    ai_voltage_field_daq_task.timing.cfg_samp_clk_timing(1000)
+    ai_voltage_task.timing.cfg_samp_clk_timing(1000)
     trigger_time = ht_datetime.now() + timedelta(seconds=10)
 
-    ai_voltage_field_daq_task.triggers.start_trigger.cfg_time_start_trig(trigger_time)
+    ai_voltage_task.triggers.start_trigger.cfg_time_start_trig(trigger_time)
 
-    when_value = ai_voltage_field_daq_task.triggers.start_trigger.trig_when
-    timescale_value = ai_voltage_field_daq_task.triggers.start_trigger.timestamp_timescale
+    when_value = ai_voltage_task.triggers.start_trigger.trig_when
+    timescale_value = ai_voltage_task.triggers.start_trigger.timestamp_timescale
     assert timescale_value == Timescale.USE_HOST
     assert when_value.year == trigger_time.year
     assert when_value.month == trigger_time.month
@@ -34,20 +34,20 @@ def test___default_arguments___cfg_time_start_trig___no_errors(
 
 
 def test___arguments_provided___cfg_time_start_trig___no_errors(
-    ai_voltage_field_daq_task: Task,
+    ai_voltage_task: Task,
 ):
-    ai_voltage_field_daq_task.timing.cfg_samp_clk_timing(1000)
+    ai_voltage_task.timing.cfg_samp_clk_timing(1000)
     trigger_time = ht_datetime.now() + timedelta(seconds=10)
     # simulated devices don't support setting timescale to USE_IO_DEVICE
     timescale = Timescale.USE_HOST
 
-    ai_voltage_field_daq_task.triggers.start_trigger.cfg_time_start_trig(trigger_time, timescale)
+    ai_voltage_task.triggers.start_trigger.cfg_time_start_trig(trigger_time, timescale)
 
-    when_value = ai_voltage_field_daq_task.triggers.start_trigger.trig_when
+    when_value = ai_voltage_task.triggers.start_trigger.trig_when
     assert when_value.year == trigger_time.year
     assert when_value.month == trigger_time.month
     assert when_value.day == trigger_time.day
     assert when_value.hour == trigger_time.hour
     assert when_value.minute == trigger_time.minute
     assert when_value.second == trigger_time.second
-    assert ai_voltage_field_daq_task.triggers.start_trigger.timestamp_timescale == timescale
+    assert ai_voltage_task.triggers.start_trigger.timestamp_timescale == timescale
