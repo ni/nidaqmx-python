133ba442c7888b625e8cb11dc8c7c5b3190d96ef
deooi <deborah.ooi@ni.com>
2024-02-28 23:13:09 -0800

    Address comments

--- generated/nidaqmx/_time.py
+++ generated/nidaqmx/_time.py
@@ -9,7 +9,7 @@ from typing import Optional, Union
 from zoneinfo import ZoneInfo
 
 # theoretically the same as astimezone(), but with support for dates before 1970
-def _convert_to_desired_timezone(expected_time_utc: Union[std_datetime, ht_datetime], tzinfo: Optional[dt_tzinfo] = None):
+def _convert_to_desired_timezone(expected_time_utc: Union[std_datetime, ht_datetime], tzinfo: Optional[dt_tzinfo] = None) -> Union[std_datetime, ht_datetime]:
     # if timezone matches, no need to do conversion
     if expected_time_utc.tzinfo is tzinfo:
         return expected_time_utc
@@ -25,9 +25,13 @@ def _convert_to_desired_timezone(expected_time_utc: Union[std_datetime, ht_datet
         return(desired_expected_time)
 
     # if the tzinfo passed in is a timedelta function, then we don't need to consider daylight savings
-    elif isinstance(tzinfo, dt_tzinfo) and tzinfo.utcoffset(None) is not None:
+    elif tzinfo.utcoffset(None) is not None:
         current_time_utc = ht_datetime.now(timezone.utc)
         desired_timezone_offset = current_time_utc.astimezone(tz=tzinfo).utcoffset()
         desired_expected_time = expected_time_utc + desired_timezone_offset
         new_datetime = desired_expected_time.replace(tzinfo=tzinfo)
         return new_datetime
+
+    # if the tzinfo passed in is none of the above, fall back to original astimezone()
+    else:
+        return expected_time_utc.astimezone(tzinfo)
