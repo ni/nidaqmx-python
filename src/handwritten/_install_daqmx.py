66bf55bcca8b711d01399109319c3a9ad8f47f71
dharaniprakashkm <dharaniprakashkm@gmail.com>
2024-03-09 18:26:44 +0530

    Incorporate review comments

--- src/handwritten/_install_daqmx.py
+++ src/handwritten/_install_daqmx.py
@@ -7,7 +7,8 @@ import winreg
 import sys
 import logging
 import click
-from typing import List, Tuple
+import errno
+from typing import List, Tuple, Optional
 
 if sys.platform.startswith("win"):
    import winreg
@@ -91,19 +92,20 @@ def _check_daqmx_is_installed() -> str:
         raise
 
 
-def _get_temp_file_name():
+def _get_temp_file_name() -> Optional[str]:
     """
     Generate a temporary file name.
     """
     try:
-        _, temp_file_path = tempfile.mkstemp()
-        return temp_file_path + "_daq-mx-online.exe"
+        fd, temp_file_name = tempfile.mkstemp(suffix=".exe")
+        os.close(fd)
+        return temp_file_name
     except Exception as e:
         print(f"error: Unable to create temporary file: {e}")
-        return None
+        raise
 
 
-def _get_url_and_version_for_windows_installer(metadata_url):
+def _fetch_driver_details(metadata_url: str) -> Tuple[Optional[str], Optional[str]]:
     """
     Parse the JSON data and retrieve the download link and version information.
     """
@@ -113,29 +115,28 @@ def _get_url_and_version_for_windows_installer(metadata_url):
             windows_os_meta = data.get("Windows", [])
 
         for metadata in windows_os_meta:
-            location = metadata.get("Location")
-            version = metadata.get("Version")
+            location: Optional[str] = metadata.get("Location")
+            version: Optional[str] = metadata.get("Version")
             if location and version:
                 return location, version
 
     except Exception as e:
         print(f"Unable to get download url: {e}")
-    return None, None
+    raise
 
 
-def _install_daqmx_driver(download_url, temp_file):
+def _install_daqmx_driver(download_url: str, temp_file: str) -> None:
     """
     Download and launch NI-DAQmx Driver installation in an interactive mode
     """
     try:
         urllib.request.urlretrieve(download_url, temp_file)
         result = subprocess.run([temp_file], shell=True, check=True)
-        print(result)
     except Exception as e:
         print(f"Error installing NI-DAQmx: {e}")
 
 
-def _ask_user_confirmation(question):
+def _ask_user_confirmation(question: str) -> bool:
     """
     Prompt for user confirmation
     """
@@ -147,6 +148,17 @@ def _ask_user_confirmation(question):
             return False
         else:
             print("Please enter 'yes' or 'no'.")
+def _cleanup_tempfile(temp_file: str) -> None:
+    """
+    Cleanup temporary files that we created
+    """
+    try:
+        if os.path.isfile(temp_file):
+           os.unlink(temp_file)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise
+
 
 def installdriver():
     """
@@ -155,44 +167,43 @@ def installdriver():
     If NI-DAQmx driver is already installed it will prompt for an upgrade if applicable.
     Download and launch NI-DAQmx Driver installation in an interactive mode.
     """
+    temp_file = ""
     if sys.platform.startswith("win"):
 
-        daqmx_latest_url, daqmx_latest_version = _get_url_and_version_for_windows_installer(
-            _META_DATA_URL
-        )
-        if daqmx_latest_version == None or daqmx_latest_url == None:
-            return -1
-        daqmx_installed_version = _check_daqmx_is_installed()
-        download_url, temp_file = daqmx_latest_url, _get_temp_file_name()
-        if temp_file == None:
-           return -1
-        if not daqmx_installed_version:
-            print(f"Installing NI-DAQmx version {daqmx_latest_version}")
-            _install_daqmx_driver(download_url, temp_file)
-        
-        elif _compare_version(daqmx_latest_version, daqmx_installed_version) > 0:
-            if download_url and temp_file:
-                user_question = (
-                    "Installed NI-DAQmx version is "
-                    + daqmx_installed_version
-                    + ". Latest version available is "
-                    + daqmx_latest_version
-                    + ". Do you want to upgrade?"
-                )
-                user_confirmation = _ask_user_confirmation(user_question)
-                if user_confirmation:
-                    print(f"Installing NI-DAQmx version {daqmx_latest_version}")
-                    _install_daqmx_driver(download_url, temp_file)
-                else:
-                    print(f"Skipping Upgrade.")
-        
-            else:
-                print("Installation aborted due to missing download URL")
-        else:
-            print(
-                f"Installed NI-DAQmx version {daqmx_installed_version} is up-to-date. Skipping installation."
+        try:
+            daqmx_latest_url, daqmx_latest_version = _fetch_driver_details(
+                _META_DATA_URL
             )
-        
+            daqmx_installed_version = _check_daqmx_is_installed()
+            download_url, temp_file = daqmx_latest_url, _get_temp_file_name()
+            if not daqmx_installed_version:
+                print(f"Installing NI-DAQmx version {daqmx_latest_version}")
+                _install_daqmx_driver(download_url, temp_file)
+            
+            elif _compare_version(daqmx_latest_version, daqmx_installed_version) > 0:
+                if download_url and temp_file:
+                    user_question = (
+                        "Installed NI-DAQmx version is "
+                        + daqmx_installed_version
+                        + ". Latest version available is "
+                        + daqmx_latest_version
+                        + ". Do you want to upgrade?"
+                    )
+                    user_confirmation = _ask_user_confirmation(user_question)
+                    if user_confirmation:
+                        print(f"Installing NI-DAQmx version {daqmx_latest_version}")
+                        _install_daqmx_driver(download_url, temp_file)
+                    else:
+                        print(f"Skipping Upgrade.")
+            
+                else:
+                    print("Installation aborted due to missing download URL")
+            else:
+                print(
+                    f"Installed NI-DAQmx version {daqmx_installed_version} is up-to-date. Skipping installation."
+                )
+        finally:
+            _cleanup_tempfile(temp_file)
     else:
         print("Unsupported OS detected.")
 
