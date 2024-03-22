4f11269927d54fab5470d3ffbe14c6e6bdda7271
charitylxy <charity.loh@ni.com>
2024-03-21 19:16:45 -0500

    update init

--- generated/nidaqmx/system/collections/__init__.py
+++ generated/nidaqmx/system/collections/__init__.py
@@ -3,6 +3,10 @@ from nidaqmx.system.collections._device_collection import DeviceCollection
 from nidaqmx.system.collections._persisted_channel_collection import PersistedChannelCollection
 from nidaqmx.system.collections._persisted_scale_collection import PersistedScaleCollection
 from nidaqmx.system.collections._persisted_task_collection import PersistedTaskCollection
-from nidaqmx.system.collections._physical_channel_collection import PhysicalChannelCollection
+from nidaqmx.system.collections._physical_channel_collection import (
+    PhysicalChannelCollection, AIPhysicalChannelCollection, AOPhysicalChannelCollection, 
+    CIPhysicalChannelCollection, COPhysicalChannelCollection, DILinesCollection, 
+    DOLinesCollection, DIPortsCollection, DOPortsCollection
+)
 
-__all__ = ['DeviceCollection', 'PersistedChannelCollection', 'PersistedScaleCollection', 'PersistedTaskCollection', 'PhysicalChannelCollection']
+__all__ = ['DeviceCollection', 'PersistedChannelCollection', 'PersistedScaleCollection', 'PersistedTaskCollection', 'PhysicalChannelCollection', 'AIPhysicalChannelCollection', 'AOPhysicalChannelCollection', 'CIPhysicalChannelCollection', 'COPhysicalChannelCollection', 'DILinesCollection', 'DOLinesCollection', 'DIPortsCollection', 'DOPortsCollection',]
