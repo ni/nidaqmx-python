db5741193f5db435301342d095e2c4319da41967
Joo Liang Cheah <wayne8884@yahoo.com.sg>
2024-04-25 10:29:14 +0800

    Add and update AI examples. (#566)
    * added and updated AI examples
    
    * reworked as per given comments
    
    * fixed style error
    
    * added pprint in event example
    
    * reworked and file renamed as per suggestions
    
    * remind user before start task in event example
    
    * reduced samples per chan
    
    * reduced samples per chan for event example
    
    * print total read in cont examples
    
    * removed samps_per_chan for cont examples
    
    * added :f for single read examples

---
+++ examples/analog_in/thrmcpl_sample.py
@@ -0,0 +1,16 @@
+﻿"""Example of analog input temperature acquisition.
+
+This example demonstrates how to acquire thermocouple measurement using
+software timing.
+"""
+
+import nidaqmx
+from nidaqmx.constants import ThermocoupleType, TemperatureUnits
+
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_thrmcpl_chan(
+        "Dev1/ai0", units=TemperatureUnits.DEG_C, thermocouple_type=ThermocoupleType.K
+    )
+
+    data = task.read()
+    print(f"Acquired data: {data:f}")
