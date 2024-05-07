fcc8c1313999cee663cbd904440e8602fdbdaa0f
Charity Loh <55676809+charitylxy@users.noreply.github.com>
2024-05-07 00:21:19 -0500

    nidaqmx-python Reports Python as Runtime Environment (#574)
    * Add set runtime environment
    
    * Address comments

--- generated/nidaqmx/_library_interpreter.py
+++ generated/nidaqmx/_library_interpreter.py
@@ -2,6 +2,7 @@
 
 import ctypes
 import logging
+import platform
 import warnings
 from typing import Optional
 
@@ -11,11 +12,12 @@ from typing import List
 from nidaqmx._base_interpreter import BaseEventHandler, BaseInterpreter
 from nidaqmx._lib import lib_importer, ctypes_byte_str, c_bool32, wrapped_ndpointer
 from nidaqmx.error_codes import DAQmxErrors, DAQmxWarnings
-from nidaqmx.errors import DaqError, DaqReadError, DaqWarning, DaqWriteError
+from nidaqmx.errors import DaqError, DaqFunctionNotSupportedError, DaqReadError, DaqWarning, DaqWriteError
 from nidaqmx._lib_time import AbsoluteTime
 
 
 _logger = logging.getLogger(__name__)
+_was_runtime_environment_set = None
 
 
 class LibraryEventHandler(BaseEventHandler):
@@ -43,7 +45,22 @@ class LibraryInterpreter(BaseInterpreter):
     __slots__ = ()
 
     def __init__(self):
-        pass
+        global _was_runtime_environment_set 
+        if _was_runtime_environment_set is None:
+            try:
+                runtime_env = platform.python_implementation()
+                version = platform.python_version()
+                self.set_runtime_environment(
+                    runtime_env,
+                    version,
+                    '',
+                    ''
+                )
+            except DaqFunctionNotSupportedError:
+                pass
+            finally:
+                _was_runtime_environment_set = True
+
 
     def add_cdaq_sync_connection(self, port_list):
         cfunc = lib_importer.windll.DAQmxAddCDAQSyncConnection
@@ -5286,6 +5303,20 @@ class LibraryInterpreter(BaseInterpreter):
 
         error_code = cfunc(
             task, attribute, ctypes.c_uint64(value))
+        self.check_for_error(error_code)
+
+    def set_runtime_environment(
+            self, environment, environment_version, reserved_1, reserved_2):
+        cfunc = lib_importer.windll.DAQmxSetRuntimeEnvironment
+        if cfunc.argtypes is None:
+            with cfunc.arglock:
+                if cfunc.argtypes is None:
+                    cfunc.argtypes = [
+                        ctypes_byte_str, ctypes_byte_str, ctypes_byte_str,
+                        ctypes_byte_str]
+
+        error_code = cfunc(
+            environment, environment_version, reserved_1, reserved_2)
         self.check_for_error(error_code)
 
     def set_scale_attribute_double(self, scale_name, attribute, value):
