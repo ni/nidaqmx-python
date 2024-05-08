ba5c22d78e19907cf09f02ea8b9c2abdfc268ffa
Joo Liang Cheah <joo.liang.cheah@ni.com>
2024-05-08 18:55:24 +0800

    removed comments

--- examples/digital_out/write_dig_port.py
+++ examples/digital_out/write_dig_port.py
@@ -13,10 +13,6 @@ with nidaqmx.Task() as task:
     task.do_channels.add_do_chan("Dev1/port0", line_grouping=LineGrouping.CHAN_FOR_ALL_LINES)
     task.write(data, auto_start=True)
 
-    # number_of_samples_written = task.write(data, auto_start=True)
-    # print(f"Generate {number_of_samples_written} voltage samples.")
-    # task.wait_until_done()
-    # task.stop()
     while not task.is_task_done():
         pass
 
