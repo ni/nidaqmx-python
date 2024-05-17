6ffec7b939c9c360899b5d4d7896dccb16b50f4e
Joo Liang Cheah <wayne8884@yahoo.com.sg>
2024-05-18 02:01:34 +0800

    Add Counter Input examples (#581)
    * Add Counter Input examples
    
    * reworked as per comments
    
    * reworked on comments part 2

---
+++ examples/counter_in/cnt_dig_event.py
@@ -0,0 +1,32 @@
+﻿"""Example of counter input edge count operation.
+
+This example demonstrates how to count digital events on a
+Counter Input Channel. The Initial Count, Count Direction, and
+Edge are all configurable.
+"""
+
+import nidaqmx
+from nidaqmx.constants import CountDirection, Edge
+
+with nidaqmx.Task() as task:
+    channel = task.ci_channels.add_ci_count_edges_chan(
+        "Dev1/ctr0",
+        edge=Edge.RISING,
+        initial_count=0,
+        count_direction=CountDirection.COUNT_UP,
+    )
+    channel.ci_count_edges_term = "/Dev1/PFI8"
+
+    print("Continuously polling. Press Ctrl+C to stop.")
+    task.start()
+
+    try:
+        edge_counts = 0
+        while True:
+            edge_counts = task.read()
+            print(f"Acquired count: {edge_counts:n}", end="\r")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        task.stop()
+        print(f"\nAcquired {edge_counts:n} total counts.")
