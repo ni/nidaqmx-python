5c541750d48ad7abe01533ccb40ea6f06ec39171
Joo Liang Cheah <wayne8884@yahoo.com.sg>
2024-05-24 15:25:48 +0800

    Add continuous acquisition examples for thrmcpl and digital trigger (#587)
    * Add continuous acquisition examples for thrmcpl and digital trigger
    
    * updated thrmcpl sampling rate and docstring

---
+++ examples/analog_in/cont_thrmcpl_samples_int_clk.py
@@ -0,0 +1,38 @@
+﻿"""Example of continuous temperature acquisition.
+
+This example demonstrates how to make continuous, hardware-timed
+temperature measurement using a thermocouple.
+"""
+
+import nidaqmx
+from nidaqmx.constants import (
+    AcquisitionType,
+    CJCSource,
+    TemperatureUnits,
+    ThermocoupleType,
+)
+
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_thrmcpl_chan(
+        "Dev1/ai0",
+        units=TemperatureUnits.DEG_C,
+        thermocouple_type=ThermocoupleType.K,
+        cjc_source=CJCSource.CONSTANT_USER_VALUE,
+        cjc_val=25.0,
+    )
+    task.timing.cfg_samp_clk_timing(10.0, sample_mode=AcquisitionType.CONTINUOUS)
+    task.start()
+    print("Acquiring samples continuously. Press Ctrl+C to stop.")
+
+    try:
+        total_read = 0
+        while True:
+            data = task.read(number_of_samples_per_channel=50)
+            read = len(data)
+            total_read += read
+            print(f"Acquired data: {read} samples. Total {total_read}.", end="\r")
+    except KeyboardInterrupt:
+        pass
+    finally:
+        task.stop()
+        print(f"\nAcquired {total_read} total samples.")
--- examples/analog_in/cont_voltage_acq_int_clk_dig_start_retrig.py
+++ examples/analog_in/cont_voltage_acq_int_clk_dig_start_retrig.py
@@ -1,9 +1,7 @@
-﻿"""Example of analog input voltage acquisition with events.
+﻿"""Example of analog input voltage acquisition with retriggering.
 
-This example demonstrates how to use Every N Samples events to
-acquire a continuous amount of data using the DAQ device's
-internal clock. The Every N Samples events indicate when data is
-available from DAQmx.
+This example demonstrates how to acquire finite amounts of data
+on each digital trigger.
 """
 
 import nidaqmx
@@ -11,7 +9,7 @@ from nidaqmx.constants import AcquisitionType
 
 
 def main():
-    """Continuously acquires data using an Every N Samples event."""
+    """Acquires data on each digital trigger."""
     total_read = 0
     with nidaqmx.Task() as task:
 
@@ -25,11 +23,16 @@ def main():
             return 0
 
         task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
-        task.timing.cfg_samp_clk_timing(1000.0, sample_mode=AcquisitionType.CONTINUOUS)
+        task.timing.cfg_samp_clk_timing(
+            1000.0, sample_mode=AcquisitionType.FINITE, samps_per_chan=1000
+        )
+        task.triggers.start_trigger.cfg_dig_edge_start_trig("/Dev1/PFI0")
+        task.triggers.start_trigger.retriggerable = True
+
         task.register_every_n_samples_acquired_into_buffer_event(1000, callback)
         task.start()
 
-        input("Running task. Press Enter to stop.\n")
+        input("Acquiring samples continuously. Press Enter to stop.\n")
 
         task.stop()
         print(f"\nAcquired {total_read} total samples.")
