4b5afff041729c3cc31dbd3bbaeadfd88adfc447
Joo Liang Cheah <joo.liang.cheah@ni.com>
2024-06-12 18:48:18 +0800

    Add examples to demonstrate TDMS logging and data plotting

---
+++ examples/analog_in/voltage_acq_int_clk_plot_data.py
@@ -0,0 +1,20 @@
+﻿"""Example of generating visualizations for acquired data.
+
+This example demonstrates how to plot the acquired data.
+"""
+
+import matplotlib.pyplot as plt
+
+import nidaqmx
+from nidaqmx.constants import READ_ALL_AVAILABLE, AcquisitionType
+
+with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
+    task.timing.cfg_samp_clk_timing(1000.0, sample_mode=AcquisitionType.FINITE, samps_per_chan=50)
+
+    data = task.read(READ_ALL_AVAILABLE)
+
+    plt.plot(data)
+    plt.ylabel("Amplitude")
+    plt.title("Waveform")
+    plt.show()
