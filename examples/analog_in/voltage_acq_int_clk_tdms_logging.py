2cc1b3a73f1c93ac58f02e535a88dcbb0c591779
Joo Liang Cheah <joo.liang.cheah@ni.com>
2024-06-19 18:14:30 +0800

    updated tdms logging example

--- examples/analog_in/voltage_acq_int_clk_tdms_logging.py
+++ examples/analog_in/voltage_acq_int_clk_tdms_logging.py
@@ -21,7 +21,7 @@ from nidaqmx.constants import (
 with nidaqmx.Task() as task:
     task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
     task.timing.cfg_samp_clk_timing(1000.0, sample_mode=AcquisitionType.FINITE, samps_per_chan=10)
-    task._in_stream.configure_logging(
+    task.in_stream.configure_logging(
         "TestData.tdms", LoggingMode.LOG_AND_READ, operation=LoggingOperation.CREATE_OR_REPLACE
     )
 
