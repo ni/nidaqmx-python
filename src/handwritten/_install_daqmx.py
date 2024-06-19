e1bb641c36a74d8a39be0dedd42e2e42b5f93184
dharaniprakashkm <dharaniprakashkm@gmail.com>
2024-06-19 16:51:09 +0530

    Fix Logic for temporary directory

--- src/handwritten/_install_daqmx.py
+++ src/handwritten/_install_daqmx.py
@@ -100,8 +100,7 @@ def _get_daqmx_installed_version() -> Optional[str]:
                 version_match = re.search(r"ii\s+ni-daqmx\s+(\d+\.\d+\.\d+)", query_output)
             elif distro.id() == "opensuse" or distro.id() == "rhel":
                 version_match = re.search(r"ni-daqmx-(\d+\.\d+\.\d+)", query_output)
-            else:
-                raise click.ClickException(f"Unsupported distribution '{distro.id()}'")
+
             if version_match:
                 installed_version = version_match.group(1)
                 _logger.info("Found installed NI-DAQmx version: %s", installed_version)
@@ -152,53 +151,35 @@ def _multi_access_temp_file(
                 ) from e
 
 
-@contextlib.contextmanager
-def _multi_access_temp_folder(*, delete: bool = True) -> Generator[str, None, None]:
-    """
-    Context manager for creating a temporary folder.
-
-    """
-    try:
-        temp_folder = tempfile.TemporaryDirectory()
-        _logger.debug("Created temp folder: %s", temp_folder.name)
-    except Exception as e:
-        _logger.info("Failed to create temporary folder.", exc_info=True)
-        raise click.ClickException(
-            f"Failed to create temporary folder '{temp_folder.name}'.\nDetails: {e}"
-        ) from e
-
-    try:
-        yield temp_folder.name
-    finally:
-        if delete:
-            try:
-                _logger.debug("Deleting temp folder: %s", temp_folder.name)
-                shutil.rmtree(temp_folder.name)
-            except ValueError as e:
-                _logger.info("Failed to delete temporary folder.", exc_info=True)
-                raise click.ClickException(
-                    f"Failed to delete temporary folder '{temp_folder.name}'.\nDetails: {e}"
-                ) from e
-
-
 def _load_data(
     json_data: str, platform: str
 ) -> Tuple[Optional[str], Optional[str], Optional[str], Optional[List[str]]]:
     """
     Load data from JSON string and extract Windows metadata.
 
-    >>> _load_data('{"Windows": [{"Location": "path/to/driver", "Version": "1.0", "Release": "2024Q1"}]}')
-    ('path/to/driver', '1.0')
-
-    >>> _load_data('{"Windows": [{"Location": "path/to/driver"}]}')
+    >>> json_data = '{"Windows": [{"Location": "path/to/windows/driver", "Version": "24.0", "Release": "2024Q1", "supportedOS": ["Windows 11"]}], "Linux": []}'
+    >>> _load_data(json_data, "Windows")
+    ('path/to/windows/driver', '24.0', '2024Q1', ['Windows 11'])
+    
+    >>> json_data = '{"Windows": [], "Linux": [{"Location": "path/to/linux/driver", "Version": "24.0", "Release": "2024Q1", "supportedOS": ["ubuntu 20.04 ", "rhel 9"]}]}'
+    >>> _load_data(json_data, "Linux")
+    ('path/to/linux/driver', '24.0', '2024Q1', ['ubuntu 20.04', 'rhel 9'])
+    
+    >>> json_data = '{"Windows": [{"Location": "path/to/windows/driver", "Version": "24.0", "Release": "2024Q1", "supportedOS": ["Windows 11"]}], "Linux": []}'
+    >>> _load_data(json_data, "Linux")
     Traceback (most recent call last):
-    ...
-    click.exceptions.ClickException: Unable to fetch driver details.
-
-    >>> _load_data('{"Linux": [{"Location": "path/to/driver", "Version": "1.0", "Release": "2024Q1"}]}')
+    click.exceptions.ClickException: Unable to fetch driver details
+    
+    >>> json_data = 'invalid json'
+    >>> _load_data(json_data, "Windows")
     Traceback (most recent call last):
-    ...
-    click.exceptions.ClickException: Unable to fetch driver details.
+    click.exceptions.ClickException: Failed to parse the driver metadata.
+    Details: Expecting value: line 1 column 1 (char 0)
+    
+    >>> json_data = '{"Windows": [{"Location": "path/to/windows/driver", "Version": "24.0", "Release": "2024Q1", "supportedOS": ["Windows 11"]}], "Linux": []}'
+    >>> _load_data(json_data, "macOS")
+    Traceback (most recent call last):
+    click.exceptions.ClickException: Unsupported os 'macOS'
 
     """
     try:
@@ -281,17 +262,17 @@ def _install_daqmx_driver_linux_core(download_url: str, release: str) -> None:
                 _logger.info("Downloading Driver to %s", temp_file)
                 urllib.request.urlretrieve(download_url, temp_file)
 
-                with _multi_access_temp_folder() as temp_folder:
-                    _directory_to_extract_to = temp_folder
+                with tempfile.TemporaryDirectory() as temp_folder:
+                    directory_to_extract_to = temp_folder
 
-                    _logger.info("Unzipping the downloaded file to %s", _directory_to_extract_to)
+                    _logger.info("Unzipping the downloaded file to %s", directory_to_extract_to)
 
                     with zipfile.ZipFile(temp_file, "r") as zip_ref:
-                        zip_ref.extractall(_directory_to_extract_to)
+                        zip_ref.extractall(directory_to_extract_to)
 
                     _logger.info("Installing NI-DAQmx")
                     for command in _get_linux_installation_commands(
-                        _directory_to_extract_to, distro.id(), distro.version(), release
+                        directory_to_extract_to, distro.id(), distro.version(), release
                     ):
                         print("\nRunning command:", " ".join(command))
                         subprocess.run(command, check=True)
