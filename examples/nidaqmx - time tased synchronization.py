b4316bb8364f1833a7837844714704c471cfc170
MAnoushe-NI <139955784+MAnoushe-NI@users.noreply.github.com>
2025-02-24 11:53:36 +0100

    Add Time Based example for scheduled acquisition

--- examples/nidaqmx - time tased synchronization.py
+++ examples/nidaqmx - time tased synchronization.py
@@ -2,16 +2,15 @@ import nidaqmx
 import time
 from nidaqmx.constants import AcquisitionType
 from datetime import timedelta, datetime, timezone
-import matplotlib.pyplot as plt
 
 # Function to perform the measurement
 def perform_measurement():
     with nidaqmx.Task() as task:
-        task.ai_channels.add_ai_voltage_chan("cDAQ9189-1DF40B6Mod8/ai0")
+        task.ai_channels.add_ai_voltage_chan("Dev1/ai0")
         
         # Configure finite sampling
-        num_samples = 10000
-        task.timing.cfg_samp_clk_timing(10000, sample_mode=AcquisitionType.FINITE, samps_per_chan=num_samples)
+        num_samples = 1000
+        task.timing.cfg_samp_clk_timing(1000, sample_mode=AcquisitionType.FINITE, samps_per_chan=num_samples)
         
         # Calculate the start time (1 minute from now)
         start_time = datetime.now(timezone.utc) + timedelta(minutes=1)
@@ -23,20 +22,13 @@ def perform_measurement():
         print("Starting the task...")
         task.start()
 
-        print(nidaqmx.__version__)
+        print("Waiting for the scheduled start time...")
         # Wait until the scheduled start time
         while datetime.now(timezone.utc) < start_time:
             time.sleep(1)
         
         # Read data after the task starts
         data = task.read(number_of_samples_per_channel=num_samples)
-
-        # Plot the data
-        plt.plot(data)
-        plt.title('Measurement Data')
-        plt.xlabel('Sample Number')
-        plt.ylabel('Voltage')
-        plt.show()
 
 # Perform the measurement
 perform_measurement()
