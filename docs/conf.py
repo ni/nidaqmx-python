54dda8ed0c37f2ba37746c6540880f674336a2f7
Brad Keryan <brad.keryan@ni.com>
2025-05-01 19:05:51 -0500

    docs: Update copyright year to 2017-%Y

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,26 @@
 """Sphinx Configuration File."""
 
-import datetime
 import os
 import pathlib
 import sys
+
 import toml
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- General configuration ------------------------------------------------
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.coverage',
-              'sphinx.ext.viewcode',
-              'sphinx.ext.napoleon']
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+]
 
-source_suffix = '.rst'
+source_suffix = ".rst"
 
-master_doc = 'index'
+master_doc = "index"
 
 root_path = pathlib.Path(__file__).parent.parent
 pyproj_file = root_path / "pyproject.toml"
@@ -27,57 +29,64 @@ proj_config = toml.loads(pyproj_file.read_text())
 project = proj_config["tool"]["poetry"]["name"]
 company = "National Instruments"
 author = company
-copyright = f"{datetime.datetime.now().year}, {company}"
+copyright = f"2017-%Y, {company}"
 
 # Release is the full version, version is only the major component
 release = proj_config["tool"]["poetry"]["version"]
 version = ".".join(release.split(".")[:2])
 description = proj_config["tool"]["poetry"]["description"]
 
-language = 'en'
+language = "en"
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 todo_include_todos = False
 
 
 # -- Options for HTML output ----------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 html_static_path = []
 
 
 # -- Options for HTMLHelp output ------------------------------------------
 
-htmlhelp_basename = 'NI-DAQmxPythonAPIdoc'
+htmlhelp_basename = "NI-DAQmxPythonAPIdoc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
 
-latex_elements = {
-}
+latex_elements = {}
 
 latex_documents = [
-    (master_doc, 'NI-DAQmxPythonAPI.tex', 'NI-DAQmx Python API Documentation',
-     'National Instruments', 'manual'),
+    (
+        master_doc,
+        "NI-DAQmxPythonAPI.tex",
+        "NI-DAQmx Python API Documentation",
+        "National Instruments",
+        "manual",
+    ),
 ]
 
 
 # -- Options for manual page output ---------------------------------------
 
-man_pages = [
-    (master_doc, 'ni-daqmxpythonapi', 'NI-DAQmx Python API Documentation',
-     [author], 1)
-]
+man_pages = [(master_doc, "ni-daqmxpythonapi", "NI-DAQmx Python API Documentation", [author], 1)]
 
 
 # -- Options for Texinfo output -------------------------------------------
 
 texinfo_documents = [
-    (master_doc, 'NI-DAQmxPythonAPI', 'NI-DAQmx Python API Documentation',
-     author, 'NI-DAQmxPythonAPI', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc,
+        "NI-DAQmxPythonAPI",
+        "NI-DAQmx Python API Documentation",
+        author,
+        "NI-DAQmxPythonAPI",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
