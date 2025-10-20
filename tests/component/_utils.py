8fa992e409f5910a83788dee8197c35d7778822e
Brad Keryan <brad.keryan@ni.com>
2025-10-20 17:24:19 -0500

    tests: Fix mypy errors for _is_timestamp_close_to_now

--- tests/component/_utils.py
+++ tests/component/_utils.py
@@ -5,9 +5,10 @@ from __future__ import annotations
 from datetime import timezone
 
 from hightime import datetime as ht_datetime
+from nitypes.time.typing import AnyDateTime
 
 
-def _is_timestamp_close_to_now(timestamp: ht_datetime, tolerance_seconds: float = 1.0) -> bool:
+def _is_timestamp_close_to_now(timestamp: AnyDateTime, tolerance_seconds: float = 1.0) -> bool:
     current_time = ht_datetime.now(timezone.utc)
     time_diff = abs((timestamp - current_time).total_seconds())
     return time_diff <= tolerance_seconds
