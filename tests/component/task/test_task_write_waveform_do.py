65c4afe431e30be99497b1030d9bdf4eb4c6f14b
Mike Prosser <mike.prosser@emerson.com>
2025-12-10 13:24:51 -0600

    update task output tests

--- tests/component/task/test_task_write_waveform_do.py
+++ tests/component/task/test_task_write_waveform_do.py
@@ -14,6 +14,7 @@ from tests.component._digital_utils import (
     _create_waveforms_for_mixed_lines,
     _get_digital_data,
     _get_waveform_data,
+    _get_waveform_port_data,
 )
 
 
@@ -278,7 +279,7 @@ def test___task___write_waveform_port_uint8___outputs_match_final_values(
 
         assert samples_written == num_samples
         actual_value = di_port1_loopback_task.read()
-        assert actual_value == _get_waveform_data(waveform)[i - 1]
+        assert actual_value == _get_waveform_port_data(waveform)[i - 1]
 
 
 def test___task___write_waveform_port_uint32___outputs_match_final_values(
@@ -297,7 +298,7 @@ def test___task___write_waveform_port_uint32___outputs_match_final_values(
 
         assert samples_written == num_samples
         actual_value = di_port0_loopback_task.read()
-        assert actual_value == _get_waveform_data(waveform)[i - 1]
+        assert actual_value == _get_waveform_port_data(waveform)[i - 1]
 
 
 @pytest.mark.disable_feature_toggle(WAVEFORM_SUPPORT)
@@ -412,8 +413,8 @@ def test___task___write_waveforms_ports___outputs_match_final_values(
         actual_value = di_multi_channel_port_loopback_task.read()
         assert actual_value[0] != actual_value[1]
         assert actual_value == [
-            _get_waveform_data(waveforms[0])[-1],
-            _get_waveform_data(waveforms[1])[-1],
+            _get_waveform_port_data(waveforms[0])[-1],
+            _get_waveform_port_data(waveforms[1])[-1],
         ]
 
 
@@ -437,7 +438,7 @@ def test___task___write_waveforms_port_and_lines___outputs_match_final_values(
         assert samples_written == num_samples
         actual_value = di_multi_channel_port_and_lines_loopback_task.read()
         assert actual_value == [
-            _get_waveform_data(waveforms[0])[-1],
+            _get_waveform_port_data(waveforms[0])[-1],
             _get_waveform_data(waveforms[1])[-1],
         ]
 
