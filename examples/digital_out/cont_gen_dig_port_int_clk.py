5276c7d47cec5c42f50f3399a1f1ef01a040c356
Brad Keryan <brad.keryan@ni.com>
2025-12-17 13:28:57 -0600

    examples: Revise cont dig port output examples (#902)
    * examples: Revise cont dig port output examples
    
    * examples: Fix import order

--- examples/digital_out/cont_gen_dig_port_int_clk.py
+++ examples/digital_out/cont_gen_dig_port_int_clk.py
@@ -8,10 +8,16 @@ import nidaqmx
 from nidaqmx.constants import AcquisitionType, LineGrouping
 
 with nidaqmx.Task() as task:
-    data = [1, 2, 4, 8, 16, 32, 64, 128]
-
-    task.do_channels.add_do_chan("Dev1/port0", line_grouping=LineGrouping.CHAN_FOR_ALL_LINES)
+    chan = task.do_channels.add_do_chan("Dev1/port0", line_grouping=LineGrouping.CHAN_FOR_ALL_LINES)
     task.timing.cfg_samp_clk_timing(10.0, sample_mode=AcquisitionType.CONTINUOUS)
+
+    # Generate a walking 1's pattern.
+    data = [1 << i for i in range(chan.do_num_lines)]
+
+    print("Writing data:")
+    for i, value in enumerate(data):
+        print("sample {:2}: 0x{:0{width}X}".format(i, value, width=chan.do_num_lines // 4))
+
     task.write(data)
     task.start()
 
