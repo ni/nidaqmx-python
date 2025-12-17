3f2815600005cfa9e2776f88205a20c7b347a426
Brad Keryan <brad.keryan@emerson.com>
2025-12-17 11:57:48 -0600

    examples: Revise cont dig port output examples

--- examples/digital_out/cont_gen_dig_port_int_clk_wfm.py
+++ examples/digital_out/cont_gen_dig_port_int_clk_wfm.py
@@ -4,25 +4,23 @@ This example demonstrates how to output a continuous digital
 pattern using the DAQ device's clock.
 """
 
+import nidaqmx
 import numpy as np
+from nidaqmx.constants import AcquisitionType, LineGrouping
 from nitypes.waveform import DigitalWaveform
 
-import nidaqmx
-from nidaqmx.constants import AcquisitionType, LineGrouping
-
-np.set_printoptions(linewidth=220)  # ensure signal.data prints on a single line
+np.set_printoptions(linewidth=120)  # ensure signal.data prints on a single line
 
 with nidaqmx.Task() as task:
-    task.do_channels.add_do_chan("Dev1/port0", line_grouping=LineGrouping.CHAN_FOR_ALL_LINES)
+    chan = task.do_channels.add_do_chan("Dev1/port0", line_grouping=LineGrouping.CHAN_FOR_ALL_LINES)
     task.timing.cfg_samp_clk_timing(10.0, sample_mode=AcquisitionType.CONTINUOUS)
 
-    sample_count = 50
-    signal_count = task.do_channels[0].do_num_lines
-    waveform = DigitalWaveform(sample_count, signal_count)
-    for i in range(sample_count):
-        for j in range(signal_count):
-            waveform.signals[j].name = f"line {j:2}"
-            waveform.signals[j].data[i] = (i >> (j % 8)) & 1
+    # Generate a walking 1's pattern.
+    waveform = DigitalWaveform.from_port(
+        [1 << i for i in range(chan.do_num_lines)], mask=(1 << chan.do_num_lines) - 1
+    )
+    for signal in waveform.signals:
+        signal.name = f"line {signal.signal_index:2}"
 
     print("Writing data:")
     for signal in waveform.signals:
